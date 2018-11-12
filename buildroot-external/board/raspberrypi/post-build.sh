aa1d6621213d8c828bdfd5428c2486cd6deeb43b
Peter Steenbergen <info@j1nx.nl>
2018-11-12 13:20:09 +0100

    MycroftOS: Buildroot: Multiple changes and fixes - Make sure there is no getty being spawned on TTY1 - Switch from Dropbear to Openssh so we can disable locale being sent over

--- buildroot-external/board/raspberrypi/post-build.sh
+++ buildroot-external/board/raspberrypi/post-build.sh
@@ -4,11 +4,11 @@ set -u
 set -e
 
 # Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
-fi
+#if [ -e ${TARGET_DIR}/etc/inittab ]; then
+#    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+#	sed -i '/GENERIC_SERIAL/a\
+#tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+#fi
 
 cp -f ../buildroot-external/board/raspberrypi/config.txt ${BINARIES_DIR}/rpi-firmware/config.txt
 cp -f ../buildroot-external/board/raspberrypi/cmdline.txt ${BINARIES_DIR}/rpi-firmware/cmdline.txt
