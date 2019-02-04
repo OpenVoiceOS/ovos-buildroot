4ed4dab9c96006782cbdfdbb74ae17365ad6980f
Peter Steenbergen <info@j1nx.nl>
2019-02-04 20:34:58 +0100

    MycroftOS: Update respeaker linux drivers - Latest modules - Switch to systemd - Initial work on switching to Pulseaudio + udev rules

--- buildroot-external/package/respeaker/respeaker.mk
+++ buildroot-external/package/respeaker/respeaker.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RESPEAKER_VERSION = be0812c70be29b0666a89f22a9d403cfb4c48fca
+RESPEAKER_VERSION = 6c6b9ab7deb607aa985048ea170428729afc07a9
 RESPEAKER_SITE = $(call github,respeaker,seeed-voicecard,$(RESPEAKER_VERSION))
 RESPEAKER_LICENSE = GNU General Public License v3.0
 RESPEAKER_DEPENDENCIES = rpi-firmware rpi-userland dtc
@@ -12,10 +12,14 @@ RESPEAKER_DEPENDENCIES = rpi-firmware rpi-userland dtc
 $(eval $(kernel-module))
 
 define RESPEAKER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/respeaker/S16respeaker_modules \
-		$(TARGET_DIR)/etc/init.d/S16respeaker_modules
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/respeaker/S60seeed_voicecard \
-                $(TARGET_DIR)/etc/init.d/S60seeed_voicecard
+	$(INSTALL) -D -m 644 $(@D)/seeed-voicecard.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/seeed-voicecard.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/sysinit.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/seeed-voicecard.service \
+		$(TARGET_DIR)/etc/systemd/system/sysinit.target.wants/seeed-voicecard.service
+
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/respeaker/respeaker.conf \
+		$(TARGET_DIR)/etc/modules-load.d/respeaker.conf
 
 	mkdir -p $(TARGET_DIR)/etc/voicecard
 	rm $(@D)/dkms.conf
@@ -26,6 +30,23 @@ define RESPEAKER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/seeed-2mic-voicecard.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
         $(INSTALL) -D -m 0644 $(@D)/seeed-4mic-voicecard.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
         $(INSTALL) -D -m 0644 $(@D)/seeed-8mic-voicecard.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
+
+	mkdir -p $(TARGET_DIR)/usr/share/pulseaudio/alsa-mixer/profile-sets/
+	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_4mic/seeed-voicecard.conf \
+		$(TARGET_DIR)/usr/share/pulseaudio/alsa-mixer/profile-sets/seeed-voicecard-4mic.conf
+	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_6mic/seeed-voicecard.conf \
+                $(TARGET_DIR)/usr/share/pulseaudio/alsa-mixer/profile-sets/seeed-voicecard-8mic.conf
+	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/91-seeedvoicecard.rules \
+		$(TARGET_DIR)/etc/udev/rules.d/91-seeedvoicecard.rules
+
+	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_4mic/default.pa \
+		$(TARGET_DIR)/etc/pulse/seeed-voicecard-4mic-default.pa
+	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_4mic/daemon.conf \
+                $(TARGET_DIR)/etc/pulse/seeed-voicecard-4mic-daemon.conf
+	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_6mic/default.pa \
+                $(TARGET_DIR)/etc/pulse/seeed-voicecard-8mic-default.pa
+        $(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_6mic/daemon.conf \
+                $(TARGET_DIR)/etc/pulse/seeed-voicecard-8mic-daemon.conf
 endef
 
 $(eval $(generic-package))
