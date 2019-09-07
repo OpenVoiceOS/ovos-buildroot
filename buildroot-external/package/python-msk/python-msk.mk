b8517c20c4a82d1fc66e69f63ecdbc5eba2b6bcd
Peter Steenbergen <info@j1nx.nl>
2019-09-07 15:02:20 +0200

    MycroftOS: Bump python-msk requirement

--- buildroot-external/package/python-msk/python-msk.mk
+++ buildroot-external/package/python-msk/python-msk.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MSK_VERSION = 0.3.12
+PYTHON_MSK_VERSION = 0.3.13
 PYTHON_MSK_SOURCE = msk-$(PYTHON_MSK_VERSION).tar.gz
-PYTHON_MSK_SITE = https://files.pythonhosted.org/packages/34/91/fad24465ad7d07184d2b7e965415f62acf90e5488cfd3fec56c89430f01b
+PYTHON_MSK_SITE = https://files.pythonhosted.org/packages/28/51/8d2193b8f1471178149cdbcc1830b086e978f99598208cae2ba2741b2979
 PYTHON_MSK_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
