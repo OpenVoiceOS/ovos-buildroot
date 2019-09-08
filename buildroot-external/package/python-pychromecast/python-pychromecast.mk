23bb1b70b350cbd701d5ee4592a6508943208fde
Peter Steenbergen <info@j1nx.nl>
2019-09-08 20:31:05 +0200

    MycroftOS: Lower python-pychromecast to 3.2.2 (mistake)

--- buildroot-external/package/python-pychromecast/python-pychromecast.mk
+++ buildroot-external/package/python-pychromecast/python-pychromecast.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYCHROMECAST_VERSION = 3.2.3
+PYTHON_PYCHROMECAST_VERSION = 3.2.2
 PYTHON_PYCHROMECAST_SOURCE = PyChromecast-$(PYTHON_PYCHROMECAST_VERSION).tar.gz
-PYTHON_PYCHROMECAST_SITE = https://files.pythonhosted.org/packages/ec/94/c5fa90465f9a4f8266d4feefc3f39f3a5caa1f342825f298c638cc582ebf
+PYTHON_PYCHROMECAST_SITE = https://files.pythonhosted.org/packages/3d/56/21a75152eb64c16d9379639c408869ae15de553e25af18db3c3bbcfc4bfa
 PYTHON_PYCHROMECAST_SETUP_TYPE = setuptools
 PYTHON_PYCHROMECAST_LICENSE = MIT
 PYTHON_PYCHROMECAST_LICENSE_FILES = LICENSE
