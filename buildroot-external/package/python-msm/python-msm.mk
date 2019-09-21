2adb2aa8044d6388b68738764f5464f40b5b9924
Peter Steenbergen <info@j1nx.nl>
2019-09-21 12:13:25 +0200

    MycroftOS: Bump python-msm to 0.8.3

--- buildroot-external/package/python-msm/python-msm.mk
+++ buildroot-external/package/python-msm/python-msm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MSM_VERSION = 0.7.9
+PYTHON_MSM_VERSION = 0.8.3
 PYTHON_MSM_SOURCE = msm-$(PYTHON_MSM_VERSION).tar.gz
-PYTHON_MSM_SITE = https://files.pythonhosted.org/packages/46/e7/cd1c3771ecf644d95ef7c3ce7b53eeccccac1ad57c3ca3211f70ad02be29
+PYTHON_MSM_SITE = https://files.pythonhosted.org/packages/d5/b5/a7b53d4fc7ccbca6fe41f19c26a61c4a67a38068fe823e571b3e3519eb4b
 PYTHON_MSM_SETUP_TYPE = setuptools
 PYTHON_MSM_LICENSE = Apache-2.0
 PYTHON_MSM_LICENSE_FILES = LICENSE
