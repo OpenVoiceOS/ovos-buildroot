135dd8058b31b3f04f942a5f5e2b5357e5b15776
Peter Steenbergen <info@j1nx.nl>
2019-09-21 11:43:01 +0200

    MycroftOS: Switch python-mycroft to utilizing sha256 commit - This way the develop branch can stay up to date with mycroft dev - Future release-tags of MycroftOS will be locked at mycroft release tags

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = v19.2.14
-PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,release/$(PYTHON_MYCROFT_VERSION))
+PYTHON_MYCROFT_VERSION = 6c228d00a914d3d1ba0506cd216089f21706eb43
+PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
 
