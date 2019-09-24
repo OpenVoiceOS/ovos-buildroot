d7e7dddf58642ac67e3d3faa0f34925bc4bae9e8
Peter Steenbergen <info@j1nx.nl>
2019-09-24 17:09:56 +0200

    MycroftOS: Bump python-mycroft to latest 19.08.0 version

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 6c228d00a914d3d1ba0506cd216089f21706eb43
+PYTHON_MYCROFT_VERSION = b746f365c91098a40f389d0c277a5513031c51d5
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
