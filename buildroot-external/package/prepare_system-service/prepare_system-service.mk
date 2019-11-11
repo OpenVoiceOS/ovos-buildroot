5ccd33e78fd3b4af984339cdc6db2033378482dd
Peter Steenbergen <info@j1nx.nl>
2019-11-11 08:53:55 +0100

    MycroftOS: Different OS layout changes - Prepare system script to run at boot to prepare the system.   Now used to restore sound configuration files to default before   udev takes over and configures the system for what it sees.   Later can be used to prepare the system for read-only OS similar   as I have done during my Kodi developemtn years. - Sound config changes to default settings. - Remove forgotten wifi-setup system.

---
+++ buildroot-external/package/prepare_system-service/prepare_system-service.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# prepare_system-service
+#
+################################################################################
+
+PREPARE_SYSTEM_SERVICE_VERSION = 0.1.0
+PREPARE_SYSTEM_SERVICE_SITE = $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/prepare_system-service
+PREPARE_SYSTEM_SERVICE_SITE_METHOD = local
+PREPARE_SYSTEM_SERVICE_LICENSE = Apache License 2.0
+PREPARE_SYSTEM_SERVICE_LICENSE_FILES = LICENSE
+
+define PREPARE_SYSTEM_SERVICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/prepare_system $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -D -m 644 $(@D)/prepare_system.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/prepare_system.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/sysinit.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/prepare_system.service \
+		$(TARGET_DIR)/etc/systemd/system/sysinit.target.wants/prepare_system.service
+endef
+
+$(eval $(generic-package))
