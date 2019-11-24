24655a9e0a1a5e84defab65cdd16b905890102f8
Peter Steenbergen <info@j1nx.nl>
2019-11-24 16:17:24 +0100

    MycroftOS: Bump python-mycroft including the requirement changes.

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 17840decd5e1ca52282bfe1c63b82bb16acf4042
+PYTHON_MYCROFT_VERSION = fc7847029c368bfc4d674f2255aa01d0e6fb0cf0
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
