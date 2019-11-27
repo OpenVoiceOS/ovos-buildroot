dc787876c91d85b63439047cd65dfda9895ba078
Peter Steenbergen <info@j1nx.nl>
2019-11-27 13:40:38 +0100

    MycroftOS: Upgrade python-adapt-parser to 0.3.4

--- buildroot-external/package/python-adapt-parser/python-adapt-parser.mk
+++ buildroot-external/package/python-adapt-parser/python-adapt-parser.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ADAPT_PARSER_VERSION = 0.3.3
+PYTHON_ADAPT_PARSER_VERSION = 0.3.4
 PYTHON_ADAPT_PARSER_SOURCE = adapt-parser-$(PYTHON_ADAPT_PARSER_VERSION).tar.gz
-PYTHON_ADAPT_PARSER_SITE = https://files.pythonhosted.org/packages/d7/be/be6417e4b6c099dfdcfec8c512de1c1e11ba446e6c81f405bd313fc2d6b4
+PYTHON_ADAPT_PARSER_SITE = https://files.pythonhosted.org/packages/01/80/2a437b4b59ada21209a420e44d9597d13f3853df3f16040d3077e1a7976a
 PYTHON_ADAPT_PARSER_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
