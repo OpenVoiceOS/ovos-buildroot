19710450c5e749d5411186439dde3ebc72cb0f1b
Peter Steenbergen <info@j1nx.nl>
2019-12-03 16:13:16 +0100

    MycroftOS: Bump to latest version - remove adapt parser patch

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = fc7847029c368bfc4d674f2255aa01d0e6fb0cf0
+PYTHON_MYCROFT_VERSION = 21699294c782edfd9919999ecbf0f5de441330e4
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
