3853026e2572dc5faa74019751314086125af306
Peter Steenbergen <info@j1nx.nl>
2019-12-07 17:44:35 +0100

    MycroftOS: Bump python-mycroft to latest 19.8.4

--- buildroot-external/package/python-msk/python-msk.mk
+++ buildroot-external/package/python-msk/python-msk.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MSK_VERSION = 0.3.13
+PYTHON_MSK_VERSION = 0.3.14
 PYTHON_MSK_SOURCE = msk-$(PYTHON_MSK_VERSION).tar.gz
-PYTHON_MSK_SITE = https://files.pythonhosted.org/packages/28/51/8d2193b8f1471178149cdbcc1830b086e978f99598208cae2ba2741b2979
+PYTHON_MSK_SITE = https://files.pythonhosted.org/packages/ef/de/d21d7d7c59f54b02910da5f4e5b77af9c40f5477079bcdfd0170e5ed4517
 PYTHON_MSK_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 21699294c782edfd9919999ecbf0f5de441330e4
+PYTHON_MYCROFT_VERSION = 599fc576905b2dfc22bfdecbf9f478cc68ebd480
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
