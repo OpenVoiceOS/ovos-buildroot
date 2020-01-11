f798a00cb7576f34a6f12db1d5c417d73b9f18f6
Peter Steenbergen <info@j1nx.nl>
2020-01-11 19:16:36 +0100

    MycroftOS: Add Bluetooth Speaker functionality.

---
+++ buildroot-external/package/btspeaker/btspeaker.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# btspeaker
+#
+################################################################################
+
+BTSPEAKER_VERSION = 0.1.0
+BTSPEAKER_SITE = $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/btspeaker
+BTSPEAKER_SITE_METHOD = local
+BTSPEAKER_LICENSE = Apache License 2.0
+BTSPEAKER_LICENSE_FILES = LICENSE
+
+define BTSPEAKER_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 644 $(@D)/btspeaker.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/btspeaker.service
+
+	mkdir -p $(TARGET_DIR)/etc/bluetooth
+	$(INSTALL) -D -m 644 $(@D)/main.conf \
+		$(TARGET_DIR)/etc/bluetooth/main.conf
+	 $(INSTALL) -D -m 600 $(@D)/pin.conf \
+                $(TARGET_DIR)/etc/bluetooth/pin.conf
+endef
+
+$(eval $(generic-package))
