46d316880fb940b374afdccfca26e5b23a109a1f
Peter Steenbergen <info@j1nx.nl>
2020-04-06 10:25:45 +0200

    MycroftOS: Addition of hostname-service This little systemd service adds the last bits of the MAC address to the hostname for support of multiple MycroftOS's on the same network.

---
+++ buildroot-external/package/hostname-service/hostname-service.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# hostname-service
+#
+################################################################################
+
+HOSTNAME_SERVICE_VERSION = 0.1.0
+HOSTNAME_SERVICE_SITE = $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/hostname-service
+HOSTNAME_SERVICE_SITE_METHOD = local
+HOSTNAME_SERVICE_LICENSE = Apache License 2.0
+HOSTNAME_SERVICE_LICENSE_FILES = LICENSE
+
+define HOSTNAME_SERVICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 644 $(@D)/hostname.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/hostname.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/hostname.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/hostname.service
+endef
+
+$(eval $(generic-package))
