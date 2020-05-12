55e6e700493f5c579f2884e7c900867afca6de96
Peter Steenbergen <info@j1nx.nl>
2020-05-12 10:42:39 +0200

    MycroftOS: GUI: More work.. Plasma-framework now included. - next to go is plasma-nano - then mycroft-gui-mark-2

--- buildroot-external/package/kf5-kio/kf5-kio.mk
+++ buildroot-external/package/kf5-kio/kf5-kio.mk
@@ -10,19 +10,15 @@ KF5_KIO_SOURCE = kio-$(KF5_KIO_VERSION).tar.xz
 KF5_KIO_LICENSE = BSD-3-Clause
 KF5_KIO_LICENSE_FILES = COPYING-CMAKE-SCRIPTS
 
-KF5_KIO_DEPENDENCIES = host-pkgconf host-kf5-kcoreaddons kf5-kservice kf5-solid \
-			kf5-kjobwidgets kf5-ktextwidgets
+KF5_KIO_DEPENDENCIES = host-pkgconf kf5-kservice kf5-solid \
+			kf5-kjobwidgets kf5-ktextwidgets \
+			kf5-knotifications host-kf5-kconfig \
+			host-kf5-kcoreaddons
+
 KF5_KIO_INSTALL_STAGING = YES
 KF5_KIO_SUPPORTS_IN_SOURCE_BUILD = NO
 
 KF5_KIO_CONF_OPTS = -DBUILD_DESIGNERPLUGIN=OFF
-KF5_KIO_CONF_OPTS += -DKF5_HOST_TOOLING="$(HOST_DIR)/usr/lib/cmake"
-
-define KF5_KIO_PRE_XDG_EXPORT
-	# desktoptojson needs to find installed service type files
-	export XDG_DATA_DIRS=${HOST_DIR}/usr/share:$XDG_DATA_DIRS
-endef
-
-KF5_KIO_PRE_BUILD_HOOKS += KF5_KIO_PRE_XDG_EXPORT
+KF5_KIO_CONF_OPTS += -DKF5_HOST_TOOLING="$(HOST_DIR)/lib/x86_64-linux-gnu/cmake"
 
 $(eval $(cmake-package))
---
+++ buildroot-external/package/kf5-phonon/kf5-phonon.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# kf5-phonon
+#
+################################################################################
+
+KF5_PHONON_VERSION = 4.11.1
+KF5_PHONON_SITE = https://download.kde.org/stable/phonon/$(KF5_PHONON_VERSION)
+KF5_PHONON_SOURCE = phonon-$(KF5_PHONON_VERSION).tar.xz
+KF5_PHONON_LICENSE = BSD-3-Clause
+KF5_PHONON_LICENSE_FILES = COPYING-CMAKE-SCRIPTS
+
+KF5_PHONON_DEPENDENCIES = host-pkgconf
+KF5_PHONON_SOLID_INSTALL_STAGING = YES
+KF5_PHONON_SOLID_SUPPORTS_IN_SOURCE_BUILD = NO
+
+$(eval $(cmake-package))
