701615d3eef24245e9353de71d9ecb73dbe97076
j1nx <p.steenbergen@j1nx.nl>
2020-05-16 21:53:05 +0200

    MycroftOS: Bump gitpython to latest version.

--- buildroot-external/package/python-gitpython/python-gitpython.mk
+++ buildroot-external/package/python-gitpython/python-gitpython.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GITPYTHON_VERSION = 2.1.11
+PYTHON_GITPYTHON_VERSION = 3.1.2
 PYTHON_GITPYTHON_SOURCE = GitPython-$(PYTHON_GITPYTHON_VERSION).tar.gz
-PYTHON_GITPYTHON_SITE = https://files.pythonhosted.org/packages/4d/e8/98e06d3bc954e3c5b34e2a579ddf26255e762d21eb24fede458eff654c51
+PYTHON_GITPYTHON_SITE = https://files.pythonhosted.org/packages/36/5d/23c3f9a527a1e1c79e8622c7bb74704f6468351cd756e20f65f2ea7aba44
 PYTHON_GITPYTHON_SETUP_TYPE = setuptools
 PYTHON_GITPYTHON_LICENSE = BSD-3-Clause
 PYTHON_GITPYTHON_LICENSE_FILES = LICENSE
