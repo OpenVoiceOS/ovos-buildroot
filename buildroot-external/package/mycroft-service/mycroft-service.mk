b3d1c011efabb6f7c99a772fdf0a144abe13f156
j1nx <p.steenbergen@j1nx.nl>
2020-05-17 17:38:09 +0200

    MycroftOS: GUI: Add mycroft-gui systemd service file

--- buildroot-external/package/mycroft-service/mycroft-service.mk
+++ buildroot-external/package/mycroft-service/mycroft-service.mk
@@ -23,6 +23,8 @@ define MYCROFT_SERVICE_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/usr/lib/systemd/system/mycroft-enclosure.service
 	$(INSTALL) -D -m 644 $(@D)/mycroft-skills.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/mycroft-skills.service
+	 $(INSTALL) -D -m 644 $(@D)/mycroft-gui.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/mycroft-gui.service
 endef
 
 $(eval $(generic-package))
