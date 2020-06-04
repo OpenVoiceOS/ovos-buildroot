8dc732be6dc9520cc131acd9305c4ae386fdea30
j1nx <p.steenbergen@j1nx.nl>
2020-06-04 20:54:00 +0200

    MycroftOS: Bump mycroft to latest version

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 90d2570cc3c6e50b66ec1fa82d24b0eac9f08033
+PYTHON_MYCROFT_VERSION = 3651bdcca942e97012962937882b73bc52adbd89
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
--- buildroot-external/package/python-padatious/python-padatious.mk
+++ buildroot-external/package/python-padatious/python-padatious.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PADATIOUS_VERSION = 0.4.6
+PYTHON_PADATIOUS_VERSION = 0.4.8
 PYTHON_PADATIOUS_SOURCE = padatious-$(PYTHON_PADATIOUS_VERSION).tar.gz
-PYTHON_PADATIOUS_SITE = https://files.pythonhosted.org/packages/1f/37/0022764d88b1aacb7f42a9e3ba5250b10212d8b6cf6223c58e639d669a6c
+PYTHON_PADATIOUS_SITE = https://files.pythonhosted.org/packages/d0/e7/70a6eb34b7e67fef5b2645df2ee1f807db2b5a345e4e6adfb2660a56425b
 PYTHON_PADATIOUS_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
