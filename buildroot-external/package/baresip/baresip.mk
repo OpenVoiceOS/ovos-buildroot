0b06159f4b57e95916081fde7099837b37dac053
j1nx <p.steenbergen@j1nx.nl>
2020-06-08 10:50:46 +0200

    MycroftOS: Add libre, librem and baresip package. This so users can install the voip skill from jarbas.

---
+++ buildroot-external/package/baresip/baresip.mk
@@ -0,0 +1,59 @@
+#############################################################
+#
+# BARESIP
+#
+#############################################################
+
+BARESIP_VERSION = 0.6.5
+BARESIP_SITE = http://www.creytiv.com/pub
+BARESIP_DEPENDENCIES = libre librem zlib
+
+BARESIP_MAKE_ENV = \
+        $(TARGET_CONFIGURE_OPTS) \
+        SYSROOT=$(STAGING_DIR)/usr \
+        LD="$(TARGET_CC)" \
+        LFLAGS="$(TARGET_LDFLAGS)"
+
+BARESIP_MAKE_ARGS = \
+        LIBRE_MK="$(STAGING_DIR)/usr/share/re/re.mk" \
+        LIBRE_INC="$(STAGING_DIR)/usr/include/re" \
+        LIBRE_SO="$(STAGING_DIR)/usr/lib" \
+        LIBREM_MK="$(STAGING_DIR)/usr/share/rem/rem.mk" \
+        LIBREM_INC="$(STAGING_DIR)/usr/include/rem" \
+        LIBREM_SO="$(STAGING_DIR)/usr/lib"
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
+BARESIP_DEPENDENCIES += alsa-lib
+endif
+
+ifeq ($(BR2_PACKAGE_FFMPEG_SWSCALE),y)
+BARESIP_DEPENDENCIES += ffmpeg
+BARESIP_MAKE_ENV += \
+        CFLAGS+=-DCODEC_TYPE_VIDEO=AVMEDIA_TYPE_VIDEO
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSNDFILE),y)
+BARESIP_DEPENDENCIES += libsndfile
+endif
+
+ifeq ($(BR2_PACKAGE_SPEEX),y)
+BARESIP_DEPENDENCIES += speex
+endif
+
+ifeq ($(BR2_PACKAGE_UTIL_LINUX_LIBUUID),y)
+BARESIP_DEPENDENCIES += util-linux
+endif
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+BARESIP_DEPENDENCIES += xlib_libXext
+endif
+
+define BARESIP_BUILD_CMDS
+        $(BARESIP_MAKE_ENV) $(MAKE) $(BARESIP_MAKE_ARGS) -C $(@D) all
+endef
+
+define BARESIP_INSTALL_TARGET_CMDS
+        $(BARESIP_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(MAKE) $(BARESIP_MAKE_ARGS) -C $(@D) install
+endef
+
+$(eval $(generic-package))
---
+++ buildroot-external/package/libre/libre.mk
@@ -0,0 +1,42 @@
+#############################################################
+#
+# libre
+#
+#############################################################
+
+LIBRE_VERSION = 0.6.1
+LIBRE_SOURCE = re-$(LIBRE_VERSION).tar.gz
+LIBRE_SITE = http://www.creytiv.com/pub
+LIBRE_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LIBRE_DEPENDENCIES += openssl
+endif
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+LIBRE_DEPENDENCIES += zlib
+endif
+
+define LIBRE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) \
+		$(MAKE) -C $(@D) \
+			LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
+			LIBRE_INC=$(STAGING_DIR)/usr/include/re \
+			LIBRE_SO=$(STAGING_DIR)/usr/lib \
+			CC="$(TARGET_CC)" \
+			EXTRA_CFLAGS="$(TARGET_CFLAGS)" \
+			EXTRA_LFLAGS="-lm" \
+			DESTDIR="$(STAGING_DIR)" \
+			SYSROOT="$(STAGING_DIR)/usr" \
+			SYSROOT_ALT="$(STAGING_DIR)/usr" \
+			RELEASE=1 \
+			CROSS_COMPILE="$(TARGET_CROSS)" \
+			OS=linux \
+			all install
+endef
+
+
+define LIBRE_INSTALL_TARGET_CMDS
+        $(INSTALL) -m 644 -D $(@D)/libre.so $(TARGET_DIR)/usr/lib/libre.so
+endef
+
+$(eval $(generic-package))
