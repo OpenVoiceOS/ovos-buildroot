569c025c22458ac2208e8c546672ea93274642dd
j1nx <p.steenbergen@j1nx.nl>
2020-08-19 14:53:33 +0200

    MycroftOS: Bump python-mycroft to 20.02.4

--- buildroot-external/package/python-lingua-franca/python-lingua-franca.mk
+++ buildroot-external/package/python-lingua-franca/python-lingua-franca.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LINGUA_FRANCA_VERSION = d3444bb6578326168c038e03f2124dfb871c2a52
+PYTHON_LINGUA_FRANCA_VERSION = 507a07b528fd70831aaac7d1cb75263838b87dee
 PYTHON_LINGUA_FRANCA_SITE = $(call github,MycroftAI,lingua-franca,$(PYTHON_LINGUA_FRANCA_VERSION))
 PYTHON_LINGUA_FRANCA_SETUP_TYPE = setuptools
 PYTHON_LINGUA_FRANCA_LICENSE = Apache-2.0
--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 6f33cc0553235df483dab109778798f0e2e9fbdc
+PYTHON_MYCROFT_VERSION = 7f7c97d0f6e6dc02a3b4af95887294e80092a2eb
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
