d959ccb81c5285177c174edec79d7a4590763515
j1nx <p.steenbergen@j1nx.nl>
2020-08-21 12:11:36 +0200

    MycroftOS: Bump mycroft to the new 20.08 release.

--- buildroot-external/package/python-msm/python-msm.mk
+++ buildroot-external/package/python-msm/python-msm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MSM_VERSION = 0.8.7
+PYTHON_MSM_VERSION = 0.8.8
 PYTHON_MSM_SOURCE = msm-$(PYTHON_MSM_VERSION).tar.gz
-PYTHON_MSM_SITE = https://files.pythonhosted.org/packages/f3/51/f0be5edba5d9e7a31b0b11857bff1324916bd2dd209314b97e1eacbed766
+PYTHON_MSM_SITE = https://files.pythonhosted.org/packages/7e/42/66f2d39be2767b064d0c384b764ef18aae11344e618287f40fea5c84c866
 PYTHON_MSM_SETUP_TYPE = setuptools
 PYTHON_MSM_LICENSE = Apache-2.0
 PYTHON_MSM_LICENSE_FILES = LICENSE
--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 7f7c97d0f6e6dc02a3b4af95887294e80092a2eb
+PYTHON_MYCROFT_VERSION = 305623acd87e868797bf4988684b3c9f23147a0f
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
