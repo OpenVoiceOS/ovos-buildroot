73d5a173343ed50c4d158fd1955c721500965378
j1nx <p.steenbergen@j1nx.nl>
2020-08-24 10:31:06 +0200

    MycroftOS: Bump python-mycroft again (20.08) dev.

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 305623acd87e868797bf4988684b3c9f23147a0f
+PYTHON_MYCROFT_VERSION = 3eee84f05355918f674260f97fb6bd6494cdfcc3
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
