e2e3f52d301aa15875aabce5cc4a48180825c1f0
j1nx <p.steenbergen@j1nx.nl>
2020-08-27 13:13:42 +0200

    MycroftOS: Update and re-enable the ReSpeaker drivers.

--- buildroot-external/package/respeaker/respeaker.mk
+++ buildroot-external/package/respeaker/respeaker.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RESPEAKER_VERSION = 0ed11aa9a4b4c627ed98620026b23f8bb86afd5a
+RESPEAKER_VERSION = 89f8a766469d049833c5fac749c3fde9e3943b3e
 RESPEAKER_SITE = $(call github,HinTak,seeed-voicecard,$(RESPEAKER_VERSION))
 RESPEAKER_LICENSE = GNU General Public License v3.0
 RESPEAKER_DEPENDENCIES = rpi-firmware dtc
@@ -24,9 +24,9 @@ define RESPEAKER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_MYCROFTOS_PATH)/package/respeaker/seeed-voicecard \
 		$(TARGET_DIR)/usr/bin
 
-	$(INSTALL) -D -m 0644 $(@D)/seeed-2mic-voicecard.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
-        $(INSTALL) -D -m 0644 $(@D)/seeed-4mic-voicecard.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
-        $(INSTALL) -D -m 0644 $(@D)/seeed-8mic-voicecard.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
+	$(INSTALL) -D -m 0644 $(@D)/seeed-2mic-voicecard.dtbo $(BINARIES_DIR)/overlays/
+        $(INSTALL) -D -m 0644 $(@D)/seeed-4mic-voicecard.dtbo $(BINARIES_DIR)/overlays/
+        $(INSTALL) -D -m 0644 $(@D)/seeed-8mic-voicecard.dtbo $(BINARIES_DIR)/overlays/
 
 	mkdir -p $(TARGET_DIR)/usr/share/pulseaudio/alsa-mixer/profile-sets/
 	$(INSTALL) -D -m 0644 $(@D)/pulseaudio/pulse_config_4mic/seeed-voicecard.conf \
