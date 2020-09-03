284d5dabd6b95d436b81c92ca694d65dcb116092
j1nx <p.steenbergen@j1nx.nl>
2020-09-03 14:54:15 +0200

    MycroftOS: Bump python-mycroft to latest 20.8.0 version.

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 3eee84f05355918f674260f97fb6bd6494cdfcc3
+PYTHON_MYCROFT_VERSION = 255ac428bddaebe820f9ee10e50aa08e17d2bce9
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
