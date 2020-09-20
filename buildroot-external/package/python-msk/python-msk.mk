1ce1aa086c4a6ddab1efc38c827547a6d2cb0287
j1nx <p.steenbergen@j1nx.nl>
2020-09-20 11:35:21 +0200

    MycroftOS: Bump python-msk to 0.3.16

--- buildroot-external/package/python-msk/python-msk.mk
+++ buildroot-external/package/python-msk/python-msk.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MSK_VERSION = 0.3.15
+PYTHON_MSK_VERSION = 0.3.16
 PYTHON_MSK_SOURCE = msk-$(PYTHON_MSK_VERSION).tar.gz
-PYTHON_MSK_SITE = https://files.pythonhosted.org/packages/8a/80/5bd3efb6affe84e9626f10cfa8edf5dd87dba1ce3d136e01c694cc1fe368
+PYTHON_MSK_SITE = https://files.pythonhosted.org/packages/e3/2a/d90ef5bf2ddeee1b456bf82bc40ce816504299818eee9290ce6248b12433
 PYTHON_MSK_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
