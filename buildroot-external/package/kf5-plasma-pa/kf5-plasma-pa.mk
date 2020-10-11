2d021b669269256cb16e45347cc7a66c069e8068
j1nx <p.steenbergen@j1nx.nl>
2020-10-11 12:07:25 +0200

    Create and addition of kf5-plasma-pa and libcanberra packages

---
+++ buildroot-external/package/kf5-plasma-pa/kf5-plasma-pa.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# kf5-plasma-pa
+#
+################################################################################
+
+KF5_PLASMA_PA_VERSION = 5.18.5
+KF5_PLASMA_PA_SITE = https://download.kde.org/stable/plasma/$(KF5_PLASMA_PA_VERSION)
+KF5_PLASMA_PA_SOURCE = plasma-pa-$(KF5_PLASMA_PA_VERSION).tar.xz
+KF5_PLASMA_PA_LICENSE = BSD-3-Clause
+KF5_PLASMA_PA_LICENSE_FILES = COPYING-CMAKE-SCRIPTS
+
+KF5_PLASMA_PA_DEPENDENCIES = host-pkgconf libcanberra kf5-plasma-framework kf5-kwayland
+KF5_PLASMA_PA_INSTALL_STAGING = YES
+KF5_PLASMA_PA_SUPPORTS_IN_SOURCE_BUILD = NO
+
+KF5_PLASMA_PA_CONF_OPTS += -DKF5_HOST_TOOLING="$(HOST_DIR)/lib/x86_64-linux-gnu/cmake"
+
+$(eval $(cmake-package))
