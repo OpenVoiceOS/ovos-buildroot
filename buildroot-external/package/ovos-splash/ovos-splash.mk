bef18116f1e6347e7a59493155590e6e0405c2f1
j1nx <p.steenbergen@j1nx.nl>
2020-10-11 14:32:29 +0200

    Addition of and switch to ovos branded splash screen

---
+++ buildroot-external/package/ovos-splash/ovos-splash.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# ovos-splash
+#
+################################################################################
+
+OVOS_SPLASH_SOURCE = psplash-0a902f7cd875ccf018456451be369f05fa55f962.tar.gz
+OVOS_SPLASH_SITE = http://git.yoctoproject.org/cgit/cgit.cgi/psplash/snapshot
+OVOS_SPLASH_LICENSE = GPL-2.0+
+OVOS_SPLASH_LICENSE_FILES = COPYING
+OVOS_SPLASH_AUTORECONF = YES
+
+define OVOS_SPLASH_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/ovos-splash/ovos-splash-start.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/ovos-splash-start.service
+
+	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/ovos-splash/ovos-splash-quit.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/ovos-splash-quit.service
+endef
+
+define OVOS_SPLASH_CHANGE_IMAGE
+	cp $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/ovos-splash/psplash-colors.h $(@D)
+	cp $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/ovos-splash/psplash-config.h $(@D)
+	cp $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/ovos-splash/base-images/* $(@D)/base-images/
+endef
+
+OVOS_SPLASH_PRE_CONFIGURE_HOOKS += OVOS_SPLASH_CHANGE_IMAGE
+
+$(eval $(autotools-package))
---
+++ buildroot-external/package/ovos-splash/psplash-config.h
@@ -0,0 +1,30 @@
+/*
+ *  pslash - a lightweight framebuffer splashscreen for embedded devices.
+ *
+ *  Copyright (c) 2014 MenloSystems GmbH
+ *  Author: Olaf Mandel <o.mandel@menlosystems.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+
+#ifndef _HAVE_PSPLASH_CONFIG_H
+#define _HAVE_PSPLASH_CONFIG_H
+
+/* Text to output on program start; if undefined, output nothing */
+#ifndef PSPLASH_DISABLE_STARTUP_MSG
+#define PSPLASH_STARTUP_MSG ""
+#endif
+
+/* Bool indicating if the image is fullscreen, as opposed to split screen */
+#ifndef PSPLASH_IMG_FULLSCREEN
+#define PSPLASH_IMG_FULLSCREEN 1
+#endif
+
+/* Position of the image split from top edge, numerator of fraction */
+#define PSPLASH_IMG_SPLIT_NUMERATOR 6
+
+/* Position of the image split from top edge, denominator of fraction */
+#define PSPLASH_IMG_SPLIT_DENOMINATOR 7
+
+#endif
