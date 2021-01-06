96ddea2de338f600eaa73c37d75fa98dc01407cc
j1nx <p.steenbergen@j1nx.nl>
2021-01-06 21:53:58 +0100

    Bump mycroft-core to grab latest fixes.

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 0050bde937e1741356031d2adca1fbc0cfcb3fb7
+PYTHON_MYCROFT_VERSION = 81eae60b61071746d3f5837c1fe5b8b64ecd0e64
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
