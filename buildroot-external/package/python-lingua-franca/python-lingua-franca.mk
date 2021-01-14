ea663a074783619c900e593d222c78abd4be8f13
j1nx <p.steenbergen@j1nx.nl>
2021-01-14 20:10:03 +0100

    Bump lingua-franca to intermediate version 0.2.2+

--- buildroot-external/package/python-lingua-franca/python-lingua-franca.mk
+++ buildroot-external/package/python-lingua-franca/python-lingua-franca.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LINGUA_FRANCA_VERSION = 507a07b528fd70831aaac7d1cb75263838b87dee
+PYTHON_LINGUA_FRANCA_VERSION = 481535b1e5c4e34cb95e47732ee278a891773720
 PYTHON_LINGUA_FRANCA_SITE = $(call github,MycroftAI,lingua-franca,$(PYTHON_LINGUA_FRANCA_VERSION))
 PYTHON_LINGUA_FRANCA_SETUP_TYPE = setuptools
 PYTHON_LINGUA_FRANCA_LICENSE = Apache-2.0
