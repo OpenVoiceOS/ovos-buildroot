8df821f6c8a709ccf2ec0c3e6ccace7fba7d6a2d
j1nx <p.steenbergen@j1nx.nl>
2021-01-14 19:04:31 +0100

    Revert "Bump lingua-franca"
    This reverts commit e061c87cea67169c79ce5011f5033c13eb68b3b2.

--- buildroot-external/package/python-lingua-franca/python-lingua-franca.mk
+++ buildroot-external/package/python-lingua-franca/python-lingua-franca.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LINGUA_FRANCA_VERSION = d30424e19d5ac641f1ccd1d4d91f9d7fec2e8a31
+PYTHON_LINGUA_FRANCA_VERSION = 507a07b528fd70831aaac7d1cb75263838b87dee
 PYTHON_LINGUA_FRANCA_SITE = $(call github,MycroftAI,lingua-franca,$(PYTHON_LINGUA_FRANCA_VERSION))
 PYTHON_LINGUA_FRANCA_SETUP_TYPE = setuptools
 PYTHON_LINGUA_FRANCA_LICENSE = Apache-2.0
