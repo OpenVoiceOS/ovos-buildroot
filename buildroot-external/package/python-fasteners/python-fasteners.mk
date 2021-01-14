dec6568ba2b370937ccd4483d128f200e36d728d
j1nx <p.steenbergen@j1nx.nl>
2021-01-14 20:27:15 +0100

    Downgrade accidentally upgraded python-fasteners

--- buildroot-external/package/python-fasteners/python-fasteners.mk
+++ buildroot-external/package/python-fasteners/python-fasteners.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FASTENERS_VERSION = 0.16
+PYTHON_FASTENERS_VERSION = 0.14.1
 PYTHON_FASTENERS_SOURCE = fasteners-$(PYTHON_FASTENERS_VERSION).tar.gz
-PYTHON_FASTENERS_SITE = https://files.pythonhosted.org/packages/d1/8f/a6c06f9bce5691a40283e52b92ec1522d6863951e738a31b109bf6bf2002
+PYTHON_FASTENERS_SITE = https://files.pythonhosted.org/packages/f4/6f/41b835c9bf69b03615630f8a6f6d45dafbec95eb4e2bb816638f043552b2
 PYTHON_FASTENERS_SETUP_TYPE = setuptools
 PYTHON_FASTENERS_LICENSE = Apache-2.0
 PYTHON_FASTENERS_LICENSE_FILES = LICENSE
