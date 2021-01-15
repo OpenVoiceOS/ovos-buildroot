24d8eb2b9e6738501548254cdf64c0e5408b8064
j1nx <p.steenbergen@j1nx.nl>
2021-01-15 10:06:04 +0100

    Bump embedded gui package

--- buildroot-external/package/mycroft-embedded-shell/mycroft-embedded-shell.mk
+++ buildroot-external/package/mycroft-embedded-shell/mycroft-embedded-shell.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_EMBEDDED_SHELL_VERSION = 1f7fab8494d049bca1c5341f7f70109363fd9252
+MYCROFT_EMBEDDED_SHELL_VERSION = 2652bece521dc0280454dcae774084efffcc398b
 MYCROFT_EMBEDDED_SHELL_SITE = $(call github,notmart,mycroft-embedded-shell,$(MYCROFT_EMBEDDED_SHELL_VERSION))
 MYCROFT_EMBEDDED_SHELL_LICENSE = Apache License 2.0
 
