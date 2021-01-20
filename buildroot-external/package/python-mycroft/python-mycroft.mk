34b16b34ed5fb5eb5f74ce42ff8f088df592bbf7
j1nx <p.steenbergen@j1nx.nl>
2021-01-20 10:16:08 +0100

    Bump skills and mycroft

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = e69f3517c8323df0afb6df447821c9dcca343e27
+PYTHON_MYCROFT_VERSION = 1a179dacab94869079755beb83eac735e8d3efe5
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
