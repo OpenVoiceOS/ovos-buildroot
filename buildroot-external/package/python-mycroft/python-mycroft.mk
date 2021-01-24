0238263f6502208cb5c077491a77cc01c37dbb6f
j1nx <p.steenbergen@j1nx.nl>
2021-01-24 21:08:05 +0100

    Getting into shape for possible release.

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 1a179dacab94869079755beb83eac735e8d3efe5
+PYTHON_MYCROFT_VERSION = 8020e54eaff9ab064bf56dac6ff3c67da4574a97
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
--- buildroot-external/package/python-ovos-utils/python-ovos-utils.mk
+++ buildroot-external/package/python-ovos-utils/python-ovos-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_UTILS_VERSION = e61410a1ed661ba0f694369c5707e97c6bc810bb
+PYTHON_OVOS_UTILS_VERSION = baf507994dae1989899ee118e7064a7bd2912427
 PYTHON_OVOS_UTILS_SITE = $(call github,OpenVoiceOS,ovos_utils,$(PYTHON_OVOS_UTILS_VERSION))
 PYTHON_OVOS_UTILS_SETUP_TYPE = setuptools
 PYTHON_OVOS_UTILS_LICENSE_FILES = LICENSE
