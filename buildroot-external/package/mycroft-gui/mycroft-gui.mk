9e6b622d311e6b53af06f5905483981466123d84
j1nx <p.steenbergen@j1nx.nl>
2021-01-26 14:10:03 +0100

    Changes and bumps

--- buildroot-external/package/mycroft-gui/mycroft-gui.mk
+++ buildroot-external/package/mycroft-gui/mycroft-gui.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-MYCROFT_GUI_VERSION = 59cebdf574bd7500332ca529d792115520314cb6
+MYCROFT_GUI_VERSION = 424fa92034f1a6b0853b34867fdb66fb6a235fa1
 MYCROFT_GUI_SITE = $(call github,MycroftAI,mycroft-gui,$(MYCROFT_GUI_VERSION))
 MYCROFT_GUI_LICENSE = Apache License 2.0
 
 MYCROFT_GUI_INSTALL_STAGING = YES
-MYCROFT_GUI_DEPENDENCIES = host-pkgconf
+MYCROFT_GUI_DEPENDENCIES = host-pkgconf kf5-kirigami2 lottie-qml
 MYCROFT_GUI_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define MYCROFT_GUI_CHANGE_IMAGE
--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 8020e54eaff9ab064bf56dac6ff3c67da4574a97
+PYTHON_MYCROFT_VERSION = 73f0299cfa43f499419d3949fe183cb92da1387e
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
