607f7f74180d7e1c4a8141c70830ab193a225fb8
j1nx <p.steenbergen@j1nx.nl>
2021-01-28 13:59:14 +0100

    [WIP] Use systemd-growfs instead of own scripts to expand the rootfs

--- buildroot-external/package/firstboot-service/firstboot-service.mk
+++ buildroot-external/package/firstboot-service/firstboot-service.mk
@@ -11,7 +11,6 @@ FIRSTBOOT_SERVICE_LICENSE = Apache License 2.0
 FIRSTBOOT_SERVICE_LICENSE_FILES = LICENSE
 
 define FIRSTBOOT_SERVICE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/resizeSD $(TARGET_DIR)/usr/sbin/
 	$(INSTALL) -m 0755 $(@D)/firstboot $(TARGET_DIR)/usr/sbin/
 	$(INSTALL) -D -m 644 $(@D)/firstboot.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/firstboot.service
