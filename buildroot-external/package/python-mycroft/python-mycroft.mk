39ef6633f1685a912b1479674c204d11a74355f2
j1nx <p.steenbergen@j1nx.nl>
2021-02-08 11:19:49 +0100

    Bump packages and more work on sound architecture

--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = 73f0299cfa43f499419d3949fe183cb92da1387e
+PYTHON_MYCROFT_VERSION = bfd6be347f0c88aabad8f287081a860b09359f3d
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
