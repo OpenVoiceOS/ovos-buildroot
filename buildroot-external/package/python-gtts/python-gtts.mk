2984d9bfe55355dd215ad8286892603dfd3cd316
j1nx <p.steenbergen@j1nx.nl>
2021-02-17 13:59:44 +0100

    Bump mycroft-core and gtts dependency

--- buildroot-external/package/python-gtts/python-gtts.mk
+++ buildroot-external/package/python-gtts/python-gtts.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_GTTS_VERSION = 2.2.0
+PYTHON_GTTS_VERSION = 2.2.2
 PYTHON_GTTS_SOURCE = gTTS-$(PYTHON_GTTS_VERSION).tar.gz
-PYTHON_GTTS_SITE = https://files.pythonhosted.org/packages/f8/a2/826a16210ccaba75c48414428bb4ca3cc2fddee5935fc3e49a8af417af92
+PYTHON_GTTS_SITE = https://files.pythonhosted.org/packages/ec/fa/cc7d5d6669f893b976be10fe9fb2cce2be0947db9916eb3fbdabcd4a2a31
 PYTHON_GTTS_SETUP_TYPE = setuptools
 PYTHON_GTTS_LICENSE = MIT
 PYTHON_GTTS_LICENSE_FILES = LICENSE
--- buildroot-external/package/python-mycroft/python-mycroft.mk
+++ buildroot-external/package/python-mycroft/python-mycroft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_VERSION = bfd6be347f0c88aabad8f287081a860b09359f3d
+PYTHON_MYCROFT_VERSION = a976bd1094dbfba155811df1ca227b40be9422ff
 PYTHON_MYCROFT_SITE = $(call github,MycroftAI,mycroft-core,$(PYTHON_MYCROFT_VERSION))
 PYTHON_MYCROFT_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LICENSE_FILES = LICENSE
