df21d07af238dadfb11c5b014648dbcf1b338c7d
j1nx <p.steenbergen@j1nx.nl>
2021-03-29 09:28:25 +0200

    Get into shape for Buildroot 2021.02

--- buildroot-external/package/fann/fann.mk
+++ buildroot-external/package/fann/fann.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FANN_VERSION = 7ec1fc7e5bd734f1d3c89b095e630e83c86b9be1
+FANN_VERSION = a3cd24e528d6a865915a4fed6e8fac164ff8bfdc
 FANN_SITE = git://github.com/libfann/fann.git
 FANN_LICENSE = GNU Lesser General Public License v2.1
 FANN_AUTORECONF = YES
