ce7edd182748c3673b455ac108dc82ec009efd42
j1nx <p.steenbergen@j1nx.nl>
2021-04-04 17:40:29 +0200

    Switch python-msm sitemethod to git

--- buildroot-external/package/python-msm/python-msm.mk
+++ buildroot-external/package/python-msm/python-msm.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_MSM_VERSION = 0.8.8
-PYTHON_MSM_SOURCE = msm-$(PYTHON_MSM_VERSION).tar.gz
-PYTHON_MSM_SITE = https://files.pythonhosted.org/packages/7e/42/66f2d39be2767b064d0c384b764ef18aae11344e618287f40fea5c84c866
+PYTHON_MSM_VERSION = 825e910a555e1882999647d226a56734a7b75ea4
+PYTHON_MSM_SITE = $(call github,MycroftAI,mycroft-skills-manager,$(PYTHON_MSM_VERSION))
 PYTHON_MSM_SETUP_TYPE = setuptools
 PYTHON_MSM_LICENSE = Apache-2.0
 PYTHON_MSM_LICENSE_FILES = LICENSE
