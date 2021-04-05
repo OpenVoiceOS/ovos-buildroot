a8d3e23a4e89e025a4460391d3e3033850d74808
j1nx <p.steenbergen@j1nx.nl>
2021-04-05 16:03:41 +0200

    Switch over to fully XDG compliant skills directories

--- buildroot-external/package/python-pako/python-pako.mk
+++ buildroot-external/package/python-pako/python-pako.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PAKO_VERSION = 0.2.3
+PYTHON_PAKO_VERSION = 0.3.1
 PYTHON_PAKO_SOURCE = pako-$(PYTHON_PAKO_VERSION).tar.gz
-PYTHON_PAKO_SITE = https://files.pythonhosted.org/packages/96/91/bf16a2f64a6628dfbfd9218f0331fbf73655530608ddd2f8fb597fdb9458
+PYTHON_PAKO_SITE = https://files.pythonhosted.org/packages/0d/27/0c491946572618f32c174101facca4dfb7034317e045c1e3950ed4fee689
 PYTHON_PAKO_SETUP_TYPE = setuptools
 PYTHON_PAKO_LICENSE = Apache-2.0
 PYTHON_PAKO_LICENSE_FILES = LICENSE
