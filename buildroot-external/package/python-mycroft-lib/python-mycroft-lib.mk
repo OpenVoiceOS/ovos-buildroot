f9eaca4e015d7698ffd64ca136841e5ea353fce5
j1nx <p.steenbergen@j1nx.nl>
2021-04-15 09:26:29 +0200

    Bump mycroft-lib

--- buildroot-external/package/python-mycroft-lib/python-mycroft-lib.mk
+++ buildroot-external/package/python-mycroft-lib/python-mycroft-lib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_LIB_VERSION = ab6ac7713ecc2bf4eb0430ea05dfcf06bbc5dd3a
+PYTHON_MYCROFT_LIB_VERSION = 3110929346a9d0d9adb091f9f8f0c7d093ba2920
 PYTHON_MYCROFT_LIB_SITE = $(call github,HelloChatterbox,mycroft-lib,$(PYTHON_MYCROFT_LIB_VERSION))
 PYTHON_MYCROFT_LIB_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LIB_LICENSE_FILES = LICENSE
