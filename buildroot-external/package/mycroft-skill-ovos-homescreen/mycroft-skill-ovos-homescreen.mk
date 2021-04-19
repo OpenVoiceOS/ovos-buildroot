84850cffff2a83da83bb848925343cc4635bc4bc
j1nx <p.steenbergen@j1nx.nl>
2021-04-19 20:21:02 +0200

    Bump ovos-homescreen skill

--- buildroot-external/package/mycroft-skill-ovos-homescreen/mycroft-skill-ovos-homescreen.mk
+++ buildroot-external/package/mycroft-skill-ovos-homescreen/mycroft-skill-ovos-homescreen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_OVOS_HOMESCREEN_VERSION = c68f2ac63b5fc112578f89f5307a2a7b3db2da9a
+MYCROFT_SKILL_OVOS_HOMESCREEN_VERSION = 980bba91f467850c51a5f5f63c65bd366b371c33
 MYCROFT_SKILL_OVOS_HOMESCREEN_SITE = git://github.com/OpenVoiceOS/skill-ovos-homescreen
 MYCROFT_SKILL_OVOS_HOMESCREEN_SITE_METHOD = git
 MYCROFT_SKILL_OVOS_HOMESCREEN_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
