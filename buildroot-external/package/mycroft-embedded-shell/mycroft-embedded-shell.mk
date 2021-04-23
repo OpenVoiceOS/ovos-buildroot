9003c0f156a163159d299a9ae9d9a20c9088aca3
j1nx <p.steenbergen@j1nx.nl>
2021-04-23 13:58:33 +0200

    Bump mycroft-embedded-shell

--- buildroot-external/package/mycroft-embedded-shell/mycroft-embedded-shell.mk
+++ buildroot-external/package/mycroft-embedded-shell/mycroft-embedded-shell.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_EMBEDDED_SHELL_VERSION = e6a1604ecd4ed215bf06df8b27e9e6f5fc16b386
+MYCROFT_EMBEDDED_SHELL_VERSION = b7f47eac025f4aeec4bb640745457faa74c1861d
 MYCROFT_EMBEDDED_SHELL_SITE = $(call github,OpenVoiceOS,mycroft-embedded-shell,$(MYCROFT_EMBEDDED_SHELL_VERSION))
 MYCROFT_EMBEDDED_SHELL_LICENSE = Apache License 2.0
 
