a7803fcfa901909dd97bef82f008150737a9b817
j1nx <p.steenbergen@j1nx.nl>
2021-04-23 16:33:11 +0200

    Bump mycroft-skill-date-time

--- buildroot-external/package/mycroft-skill-date-time/mycroft-skill-date-time.mk
+++ buildroot-external/package/mycroft-skill-date-time/mycroft-skill-date-time.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_DATE_TIME_VERSION = 20.08
+MYCROFT_SKILL_DATE_TIME_VERSION = d64f0329a6b6307438b5d59727205753db41ec61
 MYCROFT_SKILL_DATE_TIME_SITE = git://github.com/MycroftAI/skill-date-time
 MYCROFT_SKILL_DATE_TIME_SITE_METHOD = git
 MYCROFT_SKILL_DATE_TIME_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
