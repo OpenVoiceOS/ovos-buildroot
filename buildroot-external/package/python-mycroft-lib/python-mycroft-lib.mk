57522fc8496334d4691951a06a68bfd96cca1b76
j1nx <p.steenbergen@j1nx.nl>
2021-04-26 17:24:55 +0200

    Bump python-mycroft-lib - remove enclosure assumption patch - add default config to update ntp at boot

--- buildroot-external/package/python-mycroft-lib/python-mycroft-lib.mk
+++ buildroot-external/package/python-mycroft-lib/python-mycroft-lib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_LIB_VERSION = 3110929346a9d0d9adb091f9f8f0c7d093ba2920
+PYTHON_MYCROFT_LIB_VERSION = ef64e220e78054c51550230510f2b67a54b198c7
 PYTHON_MYCROFT_LIB_SITE = $(call github,HelloChatterbox,mycroft-lib,$(PYTHON_MYCROFT_LIB_VERSION))
 PYTHON_MYCROFT_LIB_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LIB_LICENSE_FILES = LICENSE
