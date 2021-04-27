6b1a71758e9bb2b45ebde32e98078b743f558aba
j1nx <p.steenbergen@j1nx.nl>
2021-04-27 12:17:36 +0200

    Bump python-mycroft-lib to fix readiness check

--- buildroot-external/package/python-mycroft-lib/python-mycroft-lib.mk
+++ buildroot-external/package/python-mycroft-lib/python-mycroft-lib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MYCROFT_LIB_VERSION = ef64e220e78054c51550230510f2b67a54b198c7
+PYTHON_MYCROFT_LIB_VERSION = bd9429ba7b4069a0209dcce3258a52d11cc1ea1f
 PYTHON_MYCROFT_LIB_SITE = $(call github,HelloChatterbox,mycroft-lib,$(PYTHON_MYCROFT_LIB_VERSION))
 PYTHON_MYCROFT_LIB_SETUP_TYPE = setuptools
 PYTHON_MYCROFT_LIB_LICENSE_FILES = LICENSE
