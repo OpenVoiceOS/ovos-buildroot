0a9b5059f90dbdaaba6e51c5fdb5f01a9fd34ed2
j1nx <p.steenbergen@j1nx.nl>
2021-05-16 12:44:42 +0200

    Bump ovos_utils

--- buildroot-external/package/python-ovos-utils/python-ovos-utils.mk
+++ buildroot-external/package/python-ovos-utils/python-ovos-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_UTILS_VERSION = 06a6896818a81c5679146e49ddb5c5fe9a27c5bf
+PYTHON_OVOS_UTILS_VERSION = 967935fb01c0e9f9779614b1a67b7aabf3a46147
 PYTHON_OVOS_UTILS_SITE = $(call github,OpenVoiceOS,ovos_utils,$(PYTHON_OVOS_UTILS_VERSION))
 PYTHON_OVOS_UTILS_SETUP_TYPE = setuptools
 PYTHON_OVOS_UTILS_LICENSE_FILES = LICENSE
