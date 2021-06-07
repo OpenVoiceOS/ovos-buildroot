e50d3924974479041edc89917aad9037306335ed
j1nx <p.steenbergen@j1nx.nl>
2021-06-07 12:25:38 +0200

    Add mycroft-configuration to pre installed skills

---
+++ buildroot-external/package/mycroft-skill-configuration/mycroft-skill-configuration.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# mycroft-skill-configuration
+#
+################################################################################
+
+MYCROFT_SKILL_CONFIGURATION_VERSION = 9231778e959c65a4f8156923fb26d11483d72ea0
+MYCROFT_SKILL_CONFIGURATION_SITE = git://github.com/MycroftAI/skill-configuration
+MYCROFT_SKILL_CONFIGURATION_SITE_METHOD = git
+MYCROFT_SKILL_CONFIGURATION_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
+MYCROFT_SKILL_CONFIGURATION_DIRNAME = mycroft-configuration.mycroftai
+
+define MYCROFT_SKILL_CONFIGURATION_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_CONFIGURATION_DIRLOCATION)/$(MYCROFT_SKILL_CONFIGURATION_DIRNAME)
+	cp -dpfr $(@D)/* $(TARGET_DIR)/$(MYCROFT_SKILL_CONFIGURATION_DIRLOCATION)/$(MYCROFT_SKILL_CONFIGURATION_DIRNAME)
+	cp -dpfr $(MYCROFT_SKILL_CONFIGURATION_DL_DIR)/git/.git* \
+		$(TARGET_DIR)/$(MYCROFT_SKILL_CONFIGURATION_DIRLOCATION)/$(MYCROFT_SKILL_CONFIGURATION_DIRNAME)
+endef
+
+$(eval $(generic-package))
