daab22dda6a531ca4469a019c21b6f596ed1fc06
j1nx <p.steenbergen@j1nx.nl>
2021-06-07 14:05:40 +0200

    Add better-stop to pre installed skills

---
+++ buildroot-external/package/mycroft-skill-better-stop/mycroft-skill-better-stop.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# mycroft-skill-better-stop
+#
+################################################################################
+
+MYCROFT_SKILL_BETTER_STOP_VERSION = b3e9983f341a94740db7d7cbbd9533230f032969
+MYCROFT_SKILL_BETTER_STOP_SITE = git://github.com/JarbasSkills/skill-better-stop
+MYCROFT_SKILL_BETTER_STOP_SITE_METHOD = git
+MYCROFT_SKILL_BETTER_STOP_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
+MYCROFT_SKILL_BETTER_STOP_DIRNAME = skill-better-stop
+
+define MYCROFT_SKILL_BETTER_STOP_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_BETTER_STOP_DIRLOCATION)/$(MYCROFT_SKILL_BETTER_STOP_DIRNAME)
+	cp -dpfr $(@D)/* $(TARGET_DIR)/$(MYCROFT_SKILL_BETTER_STOP_DIRLOCATION)/$(MYCROFT_SKILL_BETTER_STOP_DIRNAME)
+	cp -dpfr $(MYCROFT_SKILL_BETTER_STOP_DL_DIR)/git/.git* \
+		$(TARGET_DIR)/$(MYCROFT_SKILL_BETTER_STOP_DIRLOCATION)/$(MYCROFT_SKILL_BETTER_STOP_DIRNAME)
+endef
+
+$(eval $(generic-package))
