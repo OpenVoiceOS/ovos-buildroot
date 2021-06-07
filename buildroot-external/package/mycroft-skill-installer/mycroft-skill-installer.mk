986d5e6c7f94415db900ab77c99d82c12b018234
j1nx <p.steenbergen@j1nx.nl>
2021-06-07 12:34:26 +0200

    Add skill-installer to pre installed skills

---
+++ buildroot-external/package/mycroft-skill-installer/mycroft-skill-installer.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# mycroft-skill-installer
+#
+################################################################################
+
+MYCROFT_SKILL_INSTALLER_VERSION = cea3b98808aee400ece730dbdcd8957368d50983
+MYCROFT_SKILL_INSTALLER_SITE = git://github.com/MycroftAI/skill-installer
+MYCROFT_SKILL_INSTALLER_SITE_METHOD = git
+MYCROFT_SKILL_INSTALLER_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
+MYCROFT_SKILL_INSTALLER_DIRNAME = mycroft-installer.mycroftai
+
+define MYCROFT_SKILL_INSTALLER_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_INSTALLER_DIRLOCATION)/$(MYCROFT_SKILL_INSTALLER_DIRNAME)
+	cp -dpfr $(@D)/* $(TARGET_DIR)/$(MYCROFT_SKILL_INSTALLER_DIRLOCATION)/$(MYCROFT_SKILL_INSTALLER_DIRNAME)
+	cp -dpfr $(MYCROFT_SKILL_INSTALLER_DL_DIR)/git/.git* \
+		$(TARGET_DIR)/$(MYCROFT_SKILL_INSTALLER_DIRLOCATION)/$(MYCROFT_SKILL_INSTALLER_DIRNAME)
+endef
+
+$(eval $(generic-package))
