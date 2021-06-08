0468d2ddf6946f3ad340becf7dac438b7bbdd97f
j1nx <p.steenbergen@j1nx.nl>
2021-06-08 16:28:42 +0200

    Switch skill-speak over to skill-parrot

---
+++ buildroot-external/package/mycroft-skill-parrot/mycroft-skill-parrot.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# mycroft-skill-parrot
+#
+################################################################################
+
+MYCROFT_SKILL_PARROT_VERSION = abef6ff959b622a32dfe6a2bcf8e47fe451a53e6
+MYCROFT_SKILL_PARROT_SITE = git://github.com/JarbasSkills/skill-parrot
+MYCROFT_SKILL_PARROT_SITE_METHOD = git
+MYCROFT_SKILL_PARROT_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
+MYCROFT_SKILL_PARROT_DIRNAME = skill-parrot
+
+define MYCROFT_SKILL_PARROT_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_PARROT_DIRLOCATION)/$(MYCROFT_SKILL_PARROT_DIRNAME)
+	cp -dpfr $(@D)/* $(TARGET_DIR)/$(MYCROFT_SKILL_PARROT_DIRLOCATION)/$(MYCROFT_SKILL_PARROT_DIRNAME)
+	cp -dpfr $(MYCROFT_SKILL_PARROT_DL_DIR)/git/.git* \
+		$(TARGET_DIR)/$(MYCROFT_SKILL_PARROT_DIRLOCATION)/$(MYCROFT_SKILL_PARROT_DIRNAME)
+endef
+
+$(eval $(generic-package))
