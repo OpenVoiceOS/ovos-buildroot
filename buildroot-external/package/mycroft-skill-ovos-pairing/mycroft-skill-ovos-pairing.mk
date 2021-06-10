e043c30791ee45b3decf8b6377f45d2d8788a452
j1nx <p.steenbergen@j1nx.nl>
2021-06-10 16:19:56 +0200

    Bump skill-ovos-pairing to latest version

--- buildroot-external/package/mycroft-skill-ovos-pairing/mycroft-skill-ovos-pairing.mk
+++ buildroot-external/package/mycroft-skill-ovos-pairing/mycroft-skill-ovos-pairing.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_OVOS_PAIRING_VERSION = e9d4179580b7b863680c5795651cd01fb78188b3
+MYCROFT_SKILL_OVOS_PAIRING_VERSION = e5758a19bceca528c94893479fca20a300a6c804
 MYCROFT_SKILL_OVOS_PAIRING_SITE = git://github.com/OpenVoiceOS/skill-ovos-pairing
 MYCROFT_SKILL_OVOS_PAIRING_SITE_METHOD = git
 MYCROFT_SKILL_OVOS_PAIRING_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
