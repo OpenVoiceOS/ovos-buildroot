03ed04e2809707fd73a5e44386f0c30a3285e2c9
j1nx <p.steenbergen@j1nx.nl>
2021-06-12 15:22:15 +0200

    Bump HolmesV again

--- buildroot-external/package/python-holmesv/python-holmesv.mk
+++ buildroot-external/package/python-holmesv/python-holmesv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_HOLMESV_VERSION = 302277ec47185e1728ca7b08ef8a647d1102c585
+PYTHON_HOLMESV_VERSION = 618e79c0cca108eaa83eb0f93812b31d13af0078
 PYTHON_HOLMESV_SITE = $(call github,HelloChatterbox,HolmesV,$(PYTHON_HOLMESV_VERSION))
 PYTHON_HOLMESV_SETUP_TYPE = setuptools
 PYTHON_HOLMESV_LICENSE_FILES = LICENSE
--- buildroot-external/package/python-mock-msm/python-mock-msm.mk
+++ buildroot-external/package/python-mock-msm/python-mock-msm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MOCK_MSM_VERSION = 8a2ffde324f857928cd9c69ec68e97e5e97a8a77
+PYTHON_MOCK_MSM_VERSION = 13b4847f7c8cdf46a3d5f8b1ab8ca2ab4c088f7d
 PYTHON_MOCK_MSM_SITE = $(call github,HelloChatterbox,mock-msm,$(PYTHON_MOCK_MSM_VERSION))
 PYTHON_MOCK_MSM_SETUP_TYPE = setuptools
 PYTHON_MOCK_MSM_LICENSE_FILES = LICENSE
