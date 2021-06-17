95c33e99c2c4d937fa2dc15570ba3b89f4d25fec
j1nx <p.steenbergen@j1nx.nl>
2021-06-17 23:08:37 +0200

    Bump HolmesV again

--- buildroot-external/package/python-holmesv/python-holmesv.mk
+++ buildroot-external/package/python-holmesv/python-holmesv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_HOLMESV_VERSION = 618e79c0cca108eaa83eb0f93812b31d13af0078
+PYTHON_HOLMESV_VERSION = bd0301d76fa1a57b15c9991d4b4c39504861f2ed
 PYTHON_HOLMESV_SITE = $(call github,HelloChatterbox,HolmesV,$(PYTHON_HOLMESV_VERSION))
 PYTHON_HOLMESV_SETUP_TYPE = setuptools
 PYTHON_HOLMESV_LICENSE_FILES = LICENSE
