b80a6a55d0e74f68e4c34bd7b1b21c28623d9767
j1nx <p.steenbergen@j1nx.nl>
2021-06-19 12:59:58 +0200

    Bump adapt-parser

--- buildroot-external/package/python-adapt-parser/python-adapt-parser.mk
+++ buildroot-external/package/python-adapt-parser/python-adapt-parser.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ADAPT_PARSER_VERSION = 0.3.7
+PYTHON_ADAPT_PARSER_VERSION = 0.4.1
 PYTHON_ADAPT_PARSER_SOURCE = adapt-parser-$(PYTHON_ADAPT_PARSER_VERSION).tar.gz
-PYTHON_ADAPT_PARSER_SITE = https://files.pythonhosted.org/packages/9e/db/62eeafa9ad1c0c20d6dd27de9e684c21d90a58f574e55720dc6aae6764ba
+PYTHON_ADAPT_PARSER_SITE = https://files.pythonhosted.org/packages/68/01/b14c1675cf281654485a15374a8ab7251426c07e7b9e7aa5553a9cf895b8
 PYTHON_ADAPT_PARSER_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
