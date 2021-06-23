8ed4833dbd33ac16513828928c0ac2ae0df566a6
j1nx <p.steenbergen@j1nx.nl>
2021-06-23 13:15:58 +0200

    Bump mycroft-gui

--- buildroot-external/package/mycroft-gui/mycroft-gui.mk
+++ buildroot-external/package/mycroft-gui/mycroft-gui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_GUI_VERSION = ef46f3f8761eb6ff46d09b31c3a266a453a60759
+MYCROFT_GUI_VERSION = 724c9e329ac23743a22a2728c93f62b08866ea7e
 MYCROFT_GUI_SITE = $(call github,MycroftAI,mycroft-gui,$(MYCROFT_GUI_VERSION))
 MYCROFT_GUI_LICENSE = Apache License 2.0
 
