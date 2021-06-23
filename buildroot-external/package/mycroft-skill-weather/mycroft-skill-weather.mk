1d738b5b50f9934c4855869754128f7b2a7b039e
j1nx <p.steenbergen@j1nx.nl>
2021-06-23 15:32:02 +0200

    Bump Homescreen and Weather skill

--- buildroot-external/package/mycroft-skill-weather/mycroft-skill-weather.mk
+++ buildroot-external/package/mycroft-skill-weather/mycroft-skill-weather.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_WEATHER_VERSION = c0549cb6215d04504040810eb31dcd1198627fd1
+MYCROFT_SKILL_WEATHER_VERSION = 2bde73ef876335fd703fa112ce14cab455abd5ab
 MYCROFT_SKILL_WEATHER_SITE = git://github.com/OpenVoiceOS/skill-weather
 MYCROFT_SKILL_WEATHER_SITE_METHOD = git
 MYCROFT_SKILL_WEATHER_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
