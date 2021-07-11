a8fb9b6df3c1095ee4c5b889377a44e65993bcca
j1nx <p.steenbergen@j1nx.nl>
2021-07-11 12:19:16 +0200

    Bump ovos-plugin-manager

--- buildroot-external/package/python-ovos-plugin-manager/python-ovos-plugin-manager.mk
+++ buildroot-external/package/python-ovos-plugin-manager/python-ovos-plugin-manager.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_PLUGIN_MANAGER_VERSION = 6daadd2be7ace8a36bb7c9c7d4fbabccf6a9f4a6
+PYTHON_OVOS_PLUGIN_MANAGER_VERSION = 4aff6fca5518e35de4ad220c1ae5ad3562c49325
 PYTHON_OVOS_PLUGIN_MANAGER_SITE = $(call github,OpenVoiceOS,OVOS-plugin-manager,$(PYTHON_OVOS_PLUGIN_MANAGER_VERSION))
 PYTHON_OVOS_PLUGIN_MANAGER_SETUP_TYPE = setuptools
 PYTHON_OVOS_PLUGIN_MANAGER_LICENSE_FILES = LICENSE
