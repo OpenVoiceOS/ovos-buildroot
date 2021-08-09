8253dcfa3c777ed91af2cb66cd738213b8dd0989
j1nx <p.steenbergen@j1nx.nl>
2021-08-09 15:50:23 +0200

    Bump pairing and wifi skill

--- buildroot-external/package/mycroft-skill-balena-wifi-setup/mycroft-skill-balena-wifi-setup.mk
+++ buildroot-external/package/mycroft-skill-balena-wifi-setup/mycroft-skill-balena-wifi-setup.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_BALENA_WIFI_SETUP_VERSION = 5cc9716048d6216c6cd8eddeb84c93223f8aa411
+MYCROFT_SKILL_BALENA_WIFI_SETUP_VERSION = 1bbc78f88f1480f0b6b0faf13eeda3042e0d66f9
 MYCROFT_SKILL_BALENA_WIFI_SETUP_SITE = git://github.com/OpenVoiceOS/skill-balena-wifi-setup
 MYCROFT_SKILL_BALENA_WIFI_SETUP_SITE_METHOD = git
 MYCROFT_SKILL_BALENA_WIFI_SETUP_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
