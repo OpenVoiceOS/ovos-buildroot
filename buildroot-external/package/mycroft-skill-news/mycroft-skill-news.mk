121a620b260e728fef9bc648364a9daf6eb3a23c
j1nx <p.steenbergen@j1nx.nl>
2021-08-14 21:00:35 +0200

    Bump news skill

--- buildroot-external/package/mycroft-skill-news/mycroft-skill-news.mk
+++ buildroot-external/package/mycroft-skill-news/mycroft-skill-news.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_NEWS_VERSION = 00dc2bfae1675e39b88c0dded6835e676976ba0a
+MYCROFT_SKILL_NEWS_VERSION = 0bf5acc034c879759550c8f176bb3813bd1055cd
 MYCROFT_SKILL_NEWS_SITE = git://github.com/JarbasSkills/skill-news
 MYCROFT_SKILL_NEWS_SITE_METHOD = git
 MYCROFT_SKILL_NEWS_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
