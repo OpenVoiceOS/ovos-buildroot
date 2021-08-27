771e9cb3dbc030a9ff1b41e2b4f65a9abbaa5ec6
j1nx <p.steenbergen@j1nx.nl>
2021-08-27 16:42:13 +0200

    Bump python-ovos-vlc-plugin

--- buildroot-external/package/python-ovos-vlc-plugin/python-ovos-vlc-plugin.mk
+++ buildroot-external/package/python-ovos-vlc-plugin/python-ovos-vlc-plugin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_VLC_PLUGIN_VERSION = feee64d1cc16f9adf61b593ecb5c65ab4a68cba7
+PYTHON_OVOS_VLC_PLUGIN_VERSION = 9ac496337aabd829adcabdef2e2e756bf28aa31b
 PYTHON_OVOS_VLC_PLUGIN_SITE = $(call github,OpenVoiceOS,ovos-vlc-plugin,$(PYTHON_OVOS_VLC_PLUGIN_VERSION))
 PYTHON_OVOS_VLC_PLUGIN_SETUP_TYPE = setuptools
 PYTHON_OVOS_VLC_PLUGIN_LICENSE_FILES = LICENSE
