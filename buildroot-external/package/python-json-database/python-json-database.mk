f8594434e6c1d1d7148d65208000c2dadec72e06
j1nx <p.steenbergen@j1nx.nl>
2021-08-27 18:08:10 +0200

    Bump python-json-database

--- buildroot-external/package/python-json-database/python-json-database.mk
+++ buildroot-external/package/python-json-database/python-json-database.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JSON_DATABASE_VERSION = 0.5.2
+PYTHON_JSON_DATABASE_VERSION = 0.5.6
 PYTHON_JSON_DATABASE_SOURCE = json_database-$(PYTHON_JSON_DATABASE_VERSION).tar.gz
-PYTHON_JSON_DATABASE_SITE = https://files.pythonhosted.org/packages/e1/f0/3a50ed9deed6b55697e30d874605900a5694e8653b9d9747597b1a670dee
+PYTHON_JSON_DATABASE_SITE = https://files.pythonhosted.org/packages/b1/2b/545bb5cafe2cd3876475669c12f31bb1076b609177f16d6efaeb214d038c
 PYTHON_JSON_DATABASE_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
