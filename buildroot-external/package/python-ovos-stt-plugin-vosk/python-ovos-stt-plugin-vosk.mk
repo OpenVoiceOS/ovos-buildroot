16191d8cfd909e2d29d83aa9d58abc3a6ba1403c
j1nx <p.steenbergen@j1nx.nl>
2021-09-01 20:17:24 +0200

    Bump ovos-stt-plugin-vosk

--- buildroot-external/package/python-ovos-stt-plugin-vosk/python-ovos-stt-plugin-vosk.mk
+++ buildroot-external/package/python-ovos-stt-plugin-vosk/python-ovos-stt-plugin-vosk.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_OVOS_STT_PLUGIN_VOSK_VERSION = 0.1.0
-PYTHON_OVOS_STT_PLUGIN_VOSK_SOURCE = ovos-stt-plugin-vosk-$(PYTHON_OVOS_STT_PLUGIN_VOSK_VERSION).tar.gz
-PYTHON_OVOS_STT_PLUGIN_VOSK_SITE = https://files.pythonhosted.org/packages/2c/fa/fd30cced0c7defd817c1da7234488ad0f4233fb4500d7df60d6930921f07
+PYTHON_OVOS_STT_PLUGIN_VOSK_VERSION = 188843cabcc994c1a351f3cedf87298a7368d0bc
+PYTHON_OVOS_STT_PLUGIN_VOSK_SITE = $(call github,OpenVoiceOS,ovos-stt-plugin-vosk,$(PYTHON_OVOS_STT_PLUGIN_VOSK_VERSION))
 PYTHON_OVOS_STT_PLUGIN_VOSK_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
