812e92cbde7357ecb5efffc308165b9109058fb6
j1nx <p.steenbergen@j1nx.nl>
2021-09-04 15:42:24 +0200

    Get the sj201 in line / Sound system architecture changes

--- buildroot-external/package/vocalfusion/vocalfusion.mk
+++ buildroot-external/package/vocalfusion/vocalfusion.mk
@@ -12,6 +12,13 @@ $(eval $(kernel-module))
 
 define VOCALFUSION_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/xvf3510.dtbo $(BINARIES_DIR)/overlays/
+
+	$(INSTALL) -D -m 755 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/vocalfusion/xvf3510-flash \
+                $(TARGET_DIR)/usr/sbin/xvf3510-flash
+
+	mkdir -p $(TARGET_DIR)/usr/lib/firmware/xvf3510
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/vocalfusion/app_xvf3510_int_spi_boot_v4_1_0.bin \
+                $(TARGET_DIR)/usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_1_0.bin
 endef
 
 $(eval $(generic-package))
