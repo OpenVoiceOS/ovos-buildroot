88b6ed91718dda9d45093f898d24d9aecc1ef8ae
j1nx <p.steenbergen@j1nx.nl>
2021-09-05 17:42:04 +0200

    Bump lingua-nostra

--- buildroot-external/package/python-lingua-nostra/python-lingua-nostra.mk
+++ buildroot-external/package/python-lingua-nostra/python-lingua-nostra.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LINGUA_NOSTRA_VERSION = a92df402b4b3e796748cf25eeefea5e41617ee8b
+PYTHON_LINGUA_NOSTRA_VERSION = 83f0690977a1d8cc7f04e11bfded9ee3df7abd4b
 PYTHON_LINGUA_NOSTRA_SITE = $(call github,HelloChatterbox,lingua-nostra,$(PYTHON_LINGUA_NOSTRA_VERSION))
 PYTHON_LINGUA_NOSTRA_SETUP_TYPE = setuptools
 PYTHON_LINGUA_NOSTRA_LICENSE = Apache-2.0
