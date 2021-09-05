0b603ec2f30e163f15b758eaa56987466f18fb73
j1nx <p.steenbergen@j1nx.nl>
2021-09-05 19:38:26 +0200

    Bump ovos-tts-plugin-pico

--- buildroot-external/package/python-ovos-tts-plugin-pico/python-ovos-tts-plugin-pico.mk
+++ buildroot-external/package/python-ovos-tts-plugin-pico/python-ovos-tts-plugin-pico.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_TTS_PLUGIN_PICO_VERSION = 6acd4c3863befbb9f9f9fca576ec9222166429cd
+PYTHON_OVOS_TTS_PLUGIN_PICO_VERSION = 50bdca63576d15e6f1e0cfe46be65c0264ece373
 PYTHON_OVOS_TTS_PLUGIN_PICO_SITE = $(call github,OpenVoiceOS,ovos-tts-plugin-pico,$(PYTHON_OVOS_TTS_PLUGIN_PICO_VERSION))
 PYTHON_OVOS_TTS_PLUGIN_PICO_SETUP_TYPE = setuptools
 
