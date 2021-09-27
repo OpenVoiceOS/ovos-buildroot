1e32f5124274fa0ccaa9a1fad725f4924c9bd03a
Peter Steenbergen <info@j1nx.nl>
2021-09-27 10:35:07 +0200

    Bump mycroft-embedded-shell to fix touch and font colour

--- buildroot-external/package/mycroft-embedded-shell/mycroft-embedded-shell.mk
+++ buildroot-external/package/mycroft-embedded-shell/mycroft-embedded-shell.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_EMBEDDED_SHELL_VERSION = daeeae2b288192e9346e92cbb2379f95f5fe32fc
+MYCROFT_EMBEDDED_SHELL_VERSION = 46dda0a75a304ee07d4660ce38f9081767b7be36
 MYCROFT_EMBEDDED_SHELL_SITE = $(call github,OpenVoiceOS,mycroft-embedded-shell,$(MYCROFT_EMBEDDED_SHELL_VERSION))
 MYCROFT_EMBEDDED_SHELL_LICENSE = Apache License 2.0
 
