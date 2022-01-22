0733f63ab2a642e0720e115be4a2192279286ac9
j1nx <p.steenbergen@j1nx.nl>
2022-01-22 20:33:13 +0100

    Bump combo-lock, messagebus-client and ovos-plugin-manager

--- buildroot-external/package/python-combo-lock/python-combo-lock.mk
+++ buildroot-external/package/python-combo-lock/python-combo-lock.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_COMBO_LOCK_VERSION = 0.0.1
+PYTHON_COMBO_LOCK_VERSION = 0.2.1
 PYTHON_COMBO_LOCK_SOURCE = combo_lock-$(PYTHON_COMBO_LOCK_VERSION).tar.gz
-PYTHON_COMBO_LOCK_SITE = https://files.pythonhosted.org/packages/64/85/af22ffdcbb8c4f2a487e3afd23dfff8683defdd2da0dc1efbdd702fa9f4c
+PYTHON_COMBO_LOCK_SITE = https://files.pythonhosted.org/packages/6f/a4/2721af17a3649b6716410e9f6ee80dfcc296f17d762a35659e4655a017f6
 PYTHON_COMBO_LOCK_SETUP_TYPE = setuptools
 PYTHON_COMBO_LOCK_LICENSE = apache-2.0
 
