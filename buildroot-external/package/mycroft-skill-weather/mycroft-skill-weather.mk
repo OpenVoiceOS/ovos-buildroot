0f1863c03b6db2524607fc941af2b82b3aceda3e
j1nx <p.steenbergen@j1nx.nl>
2022-01-26 08:23:29 +0100

    Bump weather skill

--- buildroot-external/package/mycroft-skill-weather/mycroft-skill-weather.mk
+++ buildroot-external/package/mycroft-skill-weather/mycroft-skill-weather.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_WEATHER_VERSION = a7e8da15954cdda403d40d22a65a7a3cc2abe6d7
+MYCROFT_SKILL_WEATHER_VERSION = 541490bce01576e2eef5650440fa1103fccdaaeb
 MYCROFT_SKILL_WEATHER_SITE = https://github.com/OpenVoiceOS/skill-weather
 MYCROFT_SKILL_WEATHER_SITE_METHOD = git
 MYCROFT_SKILL_WEATHER_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
