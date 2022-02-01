9603f267f893e9171feafb91ac219fffd6241427
j1nx <p.steenbergen@j1nx.nl>
2022-02-01 09:05:52 +0100

    Switch ovos-core back to dev branch

--- buildroot-external/package/python-ovos-core/python-ovos-core.mk
+++ buildroot-external/package/python-ovos-core/python-ovos-core.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_CORE_VERSION = 05133a2f787df9cb7bc210b11d52d071d54aa06d
+PYTHON_OVOS_CORE_VERSION = 812874224ae4b9b97fec64cfc64607570e82b75c
 PYTHON_OVOS_CORE_SITE = $(call github,OpenVoiceOS,ovos-core,$(PYTHON_OVOS_CORE_VERSION))
 PYTHON_OVOS_CORE_SETUP_TYPE = setuptools
 PYTHON_OVOS_CORE_LICENSE_FILES = LICENSE
