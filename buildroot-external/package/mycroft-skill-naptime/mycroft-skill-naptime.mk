940b4455d1daea664a7720222446026d0565aa17
j1nx <p.steenbergen@j1nx.nl>
2022-02-09 15:13:55 +0100

    Bump default installed ovos-setup and naptime skills

--- buildroot-external/package/mycroft-skill-naptime/mycroft-skill-naptime.mk
+++ buildroot-external/package/mycroft-skill-naptime/mycroft-skill-naptime.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_NAPTIME_VERSION = c202d5dbeda5714fe3cd0fb746a8f593034e70de
+MYCROFT_SKILL_NAPTIME_VERSION = cd63ca17cbfa1cc0f695a833fe3d2ec0d480392b
 MYCROFT_SKILL_NAPTIME_SITE = https://github.com/OpenVoiceOS/skill-naptime
 MYCROFT_SKILL_NAPTIME_SITE_METHOD = git
 MYCROFT_SKILL_NAPTIME_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
