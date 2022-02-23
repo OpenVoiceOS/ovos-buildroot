e5c2de2baa65a75f2319eabd08249bd9dbe64bf6
j1nx <p.steenbergen@j1nx.nl>
2022-02-23 21:53:03 +0100

    Bump ovos-stt-plugin-chromium

--- buildroot-external/package/python-ovos-stt-plugin-chromium/python-ovos-stt-plugin-chromium.mk
+++ buildroot-external/package/python-ovos-stt-plugin-chromium/python-ovos-stt-plugin-chromium.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_STT_PLUGIN_CHROMIUM_VERSION = 05806c4e4a13f33ef2e7fcad52f9679a2b777963
+PYTHON_OVOS_STT_PLUGIN_CHROMIUM_VERSION = dae84723dabe8466eb0de8b073b5680a0e83f486
 PYTHON_OVOS_STT_PLUGIN_CHROMIUM_SITE = $(call github,OpenVoiceOS,ovos-stt-plugin-chromium,$(PYTHON_OVOS_STT_PLUGIN_CHROMIUM_VERSION))
 PYTHON_OVOS_STT_PLUGIN_CHROMIUM_SETUP_TYPE = setuptools
 
