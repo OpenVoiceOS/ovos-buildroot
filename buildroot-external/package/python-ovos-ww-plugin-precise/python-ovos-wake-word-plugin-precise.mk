6376c9afb0fbbb37e267733de052806dc8401c38
j1nx <p.steenbergen@j1nx.nl>
2022-02-26 19:25:18 +0100

    Bump ovos-ww-plugin-precise

--- buildroot-external/package/python-ovos-ww-plugin-precise/python-ovos-wake-word-plugin-precise.mk
+++ buildroot-external/package/python-ovos-ww-plugin-precise/python-ovos-wake-word-plugin-precise.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_WW_PLUGIN_PRECISE_VERSION = 766bc2d1e0a71f9b0e3f6a4f2fe1fbc981f90ae2
+PYTHON_OVOS_WW_PLUGIN_PRECISE_VERSION = e3e70c6dbf036c47a72f20dcd69ce30ddc495fbb
 PYTHON_OVOS_WW_PLUGIN_PRECISE_SITE = $(call github,OpenVoiceOS,ovos-ww-plugin-precise,$(PYTHON_OVOS_WW_PLUGIN_PRECISE_VERSION))
 PYTHON_OVOS_WW_PLUGIN_PRECISE_SETUP_TYPE = setuptools
 PYTHON_OVOS_WW_PLUGIN_PRECISE_LICENSE_FILES = LICENSE
