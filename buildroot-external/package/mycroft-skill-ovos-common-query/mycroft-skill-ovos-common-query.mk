dd7dbb9ccf7fd10db06e80fbcfbfca95bed7e8db
j1nx <p.steenbergen@j1nx.nl>
2022-02-26 16:40:46 +0100

    Bump skill-ovos-common-query

--- buildroot-external/package/mycroft-skill-ovos-common-query/mycroft-skill-ovos-common-query.mk
+++ buildroot-external/package/mycroft-skill-ovos-common-query/mycroft-skill-ovos-common-query.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_OVOS_COMMON_QUERY_VERSION = 2189e60b204b9f8b96d7a64429627736b714f399
+MYCROFT_SKILL_OVOS_COMMON_QUERY_VERSION = e7fe7fda707707082b0c82d5fafb7ac8f2f98913
 MYCROFT_SKILL_OVOS_COMMON_QUERY_SITE = https://github.com/OpenVoiceOS/skill-ovos-common-query
 MYCROFT_SKILL_OVOS_COMMON_QUERY_SITE_METHOD = git
 MYCROFT_SKILL_OVOS_COMMON_QUERY_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
