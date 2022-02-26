f6da67a60829f656a0bf6b12e6b2a28f1aa44bee
j1nx <p.steenbergen@j1nx.nl>
2022-02-26 18:38:22 +0100

    Bump skill-camera

--- buildroot-external/package/mycroft-skill-camera/mycroft-skill-camera.mk
+++ buildroot-external/package/mycroft-skill-camera/mycroft-skill-camera.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_CAMERA_VERSION = 1db78ad4c06013c3eee5f1e902910a36b1bd964f
+MYCROFT_SKILL_CAMERA_VERSION = 1b6d108d41eac814cc5e46a7eb3c123791d16668
 MYCROFT_SKILL_CAMERA_SITE = https://github.com/OpenVoiceOS/skill-camera
 MYCROFT_SKILL_CAMERA_SITE_METHOD = git
 MYCROFT_SKILL_CAMERA_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
