e3154258c922462a8ae9f4b1b8d5b32c39ee0ad7
j1nx <p.steenbergen@j1nx.nl>
2022-02-26 17:01:44 +0100

    Switch and bump to our skill-personal

--- buildroot-external/package/mycroft-skill-personal/mycroft-skill-personal.mk
+++ buildroot-external/package/mycroft-skill-personal/mycroft-skill-personal.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_PERSONAL_VERSION = ed99a6a26f14383bd1a42beb29f137f1a53c4954
-MYCROFT_SKILL_PERSONAL_SITE = https://github.com/MycroftAI/skill-personal
+MYCROFT_SKILL_PERSONAL_VERSION = 62b72a5111b788b4007c76dd767b6af67a64eeef
+MYCROFT_SKILL_PERSONAL_SITE = https://github.com/OpenVoiceOS/skill-personal
 MYCROFT_SKILL_PERSONAL_SITE_METHOD = git
 MYCROFT_SKILL_PERSONAL_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
-MYCROFT_SKILL_PERSONAL_DIRNAME = skill-personal.mycroftai
+MYCROFT_SKILL_PERSONAL_DIRNAME = skill-personal.openvoiceos
 
 define MYCROFT_SKILL_PERSONAL_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_PERSONAL_DIRLOCATION)/$(MYCROFT_SKILL_PERSONAL_DIRNAME)
