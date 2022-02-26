751b698a1b364350bbc30f66de0df8dbe70ef6f2
j1nx <p.steenbergen@j1nx.nl>
2022-02-26 18:52:18 +0100

    Bump precise-lite-runner

--- buildroot-external/package/python-precise-lite-runner/python-precise-lite-runner.mk
+++ buildroot-external/package/python-precise-lite-runner/python-precise-lite-runner.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PRECISE_LITE_RUNNER_VERSION = 0.3.3
-PYTHON_PRECISE_LITE_RUNNER_SOURCE = precise_lite_runner-$(PYTHON_PRECISE_LITE_RUNNER_VERSION).tar.gz
-PYTHON_PRECISE_LITE_RUNNER_SITE = https://files.pythonhosted.org/packages/71/26/d798214472cd32f8803520672270123a00dfb6f4c6bbd8a3752c9aa411c9
+PYTHON_PRECISE_LITE_RUNNER_VERSION = aafdcff2bab7a90cd32e1b9ac16daf4b38995bc6
+PYTHON_PRECISE_LITE_RUNNER_SITE = $(call github,OpenVoiceOS,precise_lite_runner,$(PYTHON_PRECISE_LITE_RUNNER_VERSION))
 PYTHON_PRECISE_LITE_RUNNER_SETUP_TYPE = setuptools
-PYTHON_PRECISE_LITE_RUNNER_LICENSE = 
+PYTHON_PRECISE_LITE_RUNNER_LICENSE = LICENSE
 
 $(eval $(python-package))
