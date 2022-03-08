ce2f67366e38e2e6224303d59452836a38504b3c
j1nx <p.steenbergen@j1nx.nl>
2022-03-08 20:46:36 +0100

    Bump ovos-skill-manager

--- buildroot-external/package/python-ovos-skill-manager/python-ovos-skill-manager.mk
+++ buildroot-external/package/python-ovos-skill-manager/python-ovos-skill-manager.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_SKILL_MANAGER_VERSION = 9cde4e7c5d59b8a6fcd00e2e6de3ba75f6b20220
+PYTHON_OVOS_SKILL_MANAGER_VERSION = f49bfb8bd69257feaa6e495dee8d6bc8a4c912c9
 PYTHON_OVOS_SKILL_MANAGER_SITE = $(call github,OpenVoiceOS,ovos_skill_manager,$(PYTHON_OVOS_SKILL_MANAGER_VERSION))
 PYTHON_OVOS_SKILL_MANAGER_SETUP_TYPE = setuptools
 PYTHON_OVOS_SKILL_MANAGER_LICENSE_FILES = LICENSE
