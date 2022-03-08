107b7ba475fdbbe950ca372379e46aa25244cf85
j1nx <p.steenbergen@j1nx.nl>
2022-03-08 20:00:13 +0100

    Bump ovos-local-backend

--- buildroot-external/package/python-ovos-local-backend/python-ovos-local-backend.mk
+++ buildroot-external/package/python-ovos-local-backend/python-ovos-local-backend.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_LOCAL_BACKEND_VERSION = 308c51c7c9b662543ad4f18f4da71e4c3b48c945
+PYTHON_OVOS_LOCAL_BACKEND_VERSION = d9c974d14f4e2e68e4085e451928794570f6687b
 PYTHON_OVOS_LOCAL_BACKEND_SITE = $(call github,OpenVoiceOS,OVOS-local-backend,$(PYTHON_OVOS_LOCAL_BACKEND_VERSION))
 PYTHON_OVOS_LOCAL_BACKEND_SETUP_TYPE = setuptools
 
