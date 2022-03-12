b10d5566e170457e7f41cbf1c9ec5505a085225e
j1nx <p.steenbergen@j1nx.nl>
2022-03-12 09:01:06 +0100

    Bump phoneme-guesser

--- buildroot-external/package/python-phoneme-guesser/python-phoneme-guesser.mk
+++ buildroot-external/package/python-phoneme-guesser/python-phoneme-guesser.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PHONEME_GUESSER_VERSION = 0.1.0
+PYTHON_PHONEME_GUESSER_VERSION = 0.1.1
 PYTHON_PHONEME_GUESSER_SOURCE = phoneme_guesser-$(PYTHON_PHONEME_GUESSER_VERSION).tar.gz
-PYTHON_PHONEME_GUESSER_SITE = https://files.pythonhosted.org/packages/c7/ea/c5b389b6d593212935412e5f314978d860bc59c393a8c1029ada3992f4bd
+PYTHON_PHONEME_GUESSER_SITE = https://files.pythonhosted.org/packages/bc/a8/1a341ca70837374a954d46a616eed073465938728f96b325c87d73ce54fc
 PYTHON_PHONEME_GUESSER_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
