5ae08fd5850b39a85acd4783b0bd5e0fe2d05406
j1nx <p.steenbergen@j1nx.nl>
2022-03-15 21:03:53 +0100

    Initial migration to use the new enclosure client PHAL service

--- buildroot-external/package/mycroft-service/mycroft-systemd_enclosure.py
+++ buildroot-external/package/mycroft-service/mycroft-systemd_enclosure.py
@@ -21,10 +21,10 @@ n = sdnotify.SystemdNotifier()
 
 def notify_ready():
     n.notify('READY=1')
-    print('Startup of Mycroft Enclosure service complete')
+    print('Startup of Mycroft Enclosure Client service complete')
 
 def notify_stopping():
     n.notify('STOPPING=1')
-    print('Stopping the Mycroft Enclosure service')
+    print('Stopping the Mycroft Enclosure Client service')
 
 main(ready_hook=notify_ready, stopping_hook=notify_stopping)
