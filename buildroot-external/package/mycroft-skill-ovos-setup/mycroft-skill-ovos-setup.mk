214d4c000520619f8b2f615aa6761f63e0845a79
j1nx <p.steenbergen@j1nx.nl>
2022-03-17 20:13:20 +0100

    Bump skill-ovos-setup

--- buildroot-external/package/mycroft-skill-ovos-setup/mycroft-skill-ovos-setup.mk
+++ buildroot-external/package/mycroft-skill-ovos-setup/mycroft-skill-ovos-setup.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_OVOS_SETUP_VERSION = 44fc23cf2d84b870fba59fcfd6a056f65ce7ffe2
+MYCROFT_SKILL_OVOS_SETUP_VERSION = b9afe3a5af89b8d47b7c1b56fdd95e5ad191068c
 MYCROFT_SKILL_OVOS_SETUP_SITE = https://github.com/OpenVoiceOS/skill-ovos-setup
 MYCROFT_SKILL_OVOS_SETUP_SITE_METHOD = git
 MYCROFT_SKILL_OVOS_SETUP_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
