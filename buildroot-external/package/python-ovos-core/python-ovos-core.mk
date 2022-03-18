035c1231ffa39461ed1de3a17280b72d7ea76134
j1nx <p.steenbergen@j1nx.nl>
2022-03-18 14:45:54 +0100

    Bump ovos-core

--- buildroot-external/package/python-ovos-core/python-ovos-core.mk
+++ buildroot-external/package/python-ovos-core/python-ovos-core.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_CORE_VERSION = 739846f917d4525ac3d7ce7074be77be0f32fc9c
+PYTHON_OVOS_CORE_VERSION = 348535cd993a755fe91b358f5a0694a3f788d226
 PYTHON_OVOS_CORE_SITE = $(call github,OpenVoiceOS,ovos-core,$(PYTHON_OVOS_CORE_VERSION))
 PYTHON_OVOS_CORE_SETUP_TYPE = setuptools
 PYTHON_OVOS_CORE_LICENSE_FILES = LICENSE
