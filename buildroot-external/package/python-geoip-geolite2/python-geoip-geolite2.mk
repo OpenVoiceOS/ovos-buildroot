56a6e72bf6a358293536bad9d2369b225f593ff5
j1nx <p.steenbergen@j1nx.nl>
2022-03-18 22:04:35 +0100

    Add python-geoip-geolite2 and python-geoip packages

---
+++ buildroot-external/package/python-geoip-geolite2/python-geoip-geolite2.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# python-geoip-geolite2
+#
+################################################################################
+
+PYTHON_GEOIP_GEOLITE2_VERSION = 2015.0303
+PYTHON_GEOIP_GEOLITE2_SITE = https://files.pythonhosted.org/packages/df/59/3f611ecca70bc91959e3e1ec325f7835d15cc35585af71dbc6c1123be48e
+PYTHON_GEOIP_GEOLITE2_SETUP_TYPE = setuptools
+PYTHON_GEOIP_GEOLITE2_LICENSE = UNKNOWN
+
+$(eval $(python-package))
---
+++ buildroot-external/package/python-geoip/python-geoip.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-geoip
+#
+################################################################################
+
+PYTHON_GEOIP_VERSION = 1.2
+PYTHON_GEOIP_SITE = https://files.pythonhosted.org/packages/7c/65/cb04188154f7626e897d55f04c2542ba4205352f158cd925d314ad1998ef
+PYTHON_GEOIP_SETUP_TYPE = setuptools
+PYTHON_GEOIP_LICENSE = FIXME: please specify the exact BSD version
+PYTHON_GEOIP_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
