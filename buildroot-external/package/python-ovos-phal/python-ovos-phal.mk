7f0f4b9df08e9eb212d9ae792f5cc3167ee9312d
j1nx <p.steenbergen@j1nx.nl>
2022-05-13 14:09:38 +0200

    Bump python-ovos-phal

--- buildroot-external/package/python-ovos-phal/python-ovos-phal.mk
+++ buildroot-external/package/python-ovos-phal/python-ovos-phal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_PHAL_VERSION = dc85d17c724e6c973cda6ec221d9d1e8a1a79dec
+PYTHON_OVOS_PHAL_VERSION = 7c68e331707678f650c6083bb1594664021ca95a
 PYTHON_OVOS_PHAL_SITE = $(call github,OpenVoiceOS,ovos_PHAL,$(PYTHON_OVOS_PHAL_VERSION))
 PYTHON_OVOS_PHAL_SETUP_TYPE = setuptools
 PYTHON_OVOS_PHAL_LICENSE_FILES = LICENSE
