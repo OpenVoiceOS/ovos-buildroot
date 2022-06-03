fbfaee881facce4c5584ddcb21c6f23d476a6bc8
j1nx <p.steenbergen@j1nx.nl>
2022-06-03 14:24:41 +0200

    Bump python-ovos-workshop

--- buildroot-external/package/python-ovos-workshop/python-ovos-workshop.mk
+++ buildroot-external/package/python-ovos-workshop/python-ovos-workshop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_WORKSHOP_VERSION = b6fbe4fd8009cfb069c4b22df34b1ca87ec1448f
+PYTHON_OVOS_WORKSHOP_VERSION = 0ed6c8254c69094bdc2e18faf61c13eeb2a2995d
 PYTHON_OVOS_WORKSHOP_SITE = $(call github,OpenVoiceOS,OVOS-workshop,$(PYTHON_OVOS_WORKSHOP_VERSION))
 PYTHON_OVOS_WORKSHOP_SETUP_TYPE = setuptools
 PYTHON_OVOS_WORKSHOP_LICENSE_FILES = LICENSE
