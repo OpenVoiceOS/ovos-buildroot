a8c5f465824c4864c9d95bc5086b767900607728
j1nx <p.steenbergen@j1nx.nl>
2022-06-03 14:47:46 +0200

    Bump skill-wolfie

--- buildroot-external/package/mycroft-skill-wolfie/mycroft-skill-wolfie.mk
+++ buildroot-external/package/mycroft-skill-wolfie/mycroft-skill-wolfie.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_WOLFIE_VERSION = 8736018d95668ebe6c070111cabb3f1741ddfd79
-MYCROFT_SKILL_WOLFIE_SITE = https://github.com/NeonJarbas/skill-wolfie
+MYCROFT_SKILL_WOLFIE_VERSION = c5ff51a0c5390cbf579293f74e30028061453344
+MYCROFT_SKILL_WOLFIE_SITE = https://github.com/OpenVoiceOS/skill-wolfie
 MYCROFT_SKILL_WOLFIE_SITE_METHOD = git
 MYCROFT_SKILL_WOLFIE_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
-MYCROFT_SKILL_WOLFIE_DIRNAME = skill-wolfie.jarbasskills
+MYCROFT_SKILL_WOLFIE_DIRNAME = skill-wolfie.openvoiceos
 
 define MYCROFT_SKILL_WOLFIE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_WOLFIE_DIRLOCATION)/$(MYCROFT_SKILL_WOLFIE_DIRNAME)
