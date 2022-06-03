2ff7621461f32a619134e01cc6c61e52bff0e738
j1nx <p.steenbergen@j1nx.nl>
2022-06-03 15:43:59 +0200

    Bump python-neon-solvers

--- buildroot-external/package/python-neon-solvers/python-neon-solvers.mk
+++ buildroot-external/package/python-neon-solvers/python-neon-solvers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_NEON_SOLVERS_VERSION = b593b8ae9a7e7eae3b515940fa671d6a03956555
+PYTHON_NEON_SOLVERS_VERSION = c37161405cd6aa583e5623315342986cf5c829af
 PYTHON_NEON_SOLVERS_SITE = $(call github,NeonGeckoCom,neon_solvers,$(PYTHON_NEON_SOLVERS_VERSION))
 PYTHON_NEON_SOLVERS_SETUP_TYPE = setuptools
 PYTHON_NEON_SOLVERS_LICENSE_FILES = LICENSE
