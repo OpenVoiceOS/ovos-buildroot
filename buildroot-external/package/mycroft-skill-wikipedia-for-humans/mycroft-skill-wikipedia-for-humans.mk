702fbaa402705b71227a8785a9b4f2b9f104bc0f
j1nx <p.steenbergen@j1nx.nl>
2022-06-03 14:51:17 +0200

    Bump skill-wikipedia-for-humans

--- buildroot-external/package/mycroft-skill-wikipedia-for-humans/mycroft-skill-wikipedia-for-humans.mk
+++ buildroot-external/package/mycroft-skill-wikipedia-for-humans/mycroft-skill-wikipedia-for-humans.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_VERSION = 66fd1e49b45e6db263fba5248131dffa765900fc
-MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_SITE = https://github.com/JarbasSkills/skill-wikipedia-for-humans
+MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_VERSION = d37d0fd927cede5f9c040de53ebf4571c628e6e4
+MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_SITE = https://github.com/OpenVoiceOS/skill-wikipedia-for-humans
 MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_SITE_METHOD = git
 MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
-MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_DIRNAME = skill-wikipedia-for-humans.jarbasskills
+MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_DIRNAME = skill-wikipedia-for-humans.openvoiceos
 
 define MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_DIRLOCATION)/$(MYCROFT_SKILL_WIKIPEDIA_FOR_HUMANS_DIRNAME)
