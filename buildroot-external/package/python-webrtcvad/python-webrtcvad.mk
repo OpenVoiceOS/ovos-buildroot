f7107b578bb564709d770c057ea4faee6ad67ff3
j1nx <p.steenbergen@j1nx.nl>
2022-06-08 10:51:02 +0200

    Bump python-webrtcvad to latest version from github

--- buildroot-external/package/python-webrtcvad/python-webrtcvad.mk
+++ buildroot-external/package/python-webrtcvad/python-webrtcvad.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_WEBRTCVAD_VERSION = 2.0.10
-PYTHON_WEBRTCVAD_SOURCE = webrtcvad-$(PYTHON_WEBRTCVAD_VERSION).tar.gz
-PYTHON_WEBRTCVAD_SITE = https://files.pythonhosted.org/packages/89/34/e2de2d97f3288512b9ea56f92e7452f8207eb5a0096500badf9dfd48f5e6
+PYTHON_WEBRTCVAD_VERSION = e283ca41df3a84b0e87fb1f5cb9b21580a286b09
+PYTHON_WEBRTCVAD_SITE = $(call github,wiseman,py-webrtcvad,$(PYTHON_WEBRTCVAD_VERSION))
 PYTHON_WEBRTCVAD_SETUP_TYPE = setuptools
 PYTHON_WEBRTCVAD_LICENSE = MIT
 PYTHON_WEBRTCVAD_LICENSE_FILES = COPYING
