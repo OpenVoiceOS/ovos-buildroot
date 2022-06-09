ce86869f53e61f959348d396d3fce927c88abcf1
j1nx <p.steenbergen@j1nx.nl>
2022-06-09 12:34:35 +0200

    Fix weather skill name

--- buildroot-external/package/mycroft-skill-weather/mycroft-skill-weather.mk
+++ buildroot-external/package/mycroft-skill-weather/mycroft-skill-weather.mk
@@ -5,10 +5,10 @@
 ################################################################################
 
 MYCROFT_SKILL_WEATHER_VERSION = 9748286ff43cbe5bbf8c8ab457d58ce9668cd46e
-MYCROFT_SKILL_WEATHER_SITE = https://github.com/OpenVoiceOS/skill-weather
+MYCROFT_SKILL_WEATHER_SITE = https://github.com/OpenVoiceOS/skill-ovos-weather
 MYCROFT_SKILL_WEATHER_SITE_METHOD = git
 MYCROFT_SKILL_WEATHER_DIRLOCATION = home/mycroft/.local/share/mycroft/skills
-MYCROFT_SKILL_WEATHER_DIRNAME = skill-weather.openvoiceos
+MYCROFT_SKILL_WEATHER_DIRNAME = skill-ovos-weather.openvoiceos
 
 define MYCROFT_SKILL_WEATHER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/$(MYCROFT_SKILL_WEATHER_DIRLOCATION)/$(MYCROFT_SKILL_WEATHER_DIRNAME)
