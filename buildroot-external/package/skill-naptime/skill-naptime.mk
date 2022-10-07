f4a8bb2c9e1acf2be856d565b4f62cd405f40ebb
j1nx <p.steenbergen@j1nx.nl>
2022-10-07 08:21:40 +0200

    Add naptime skill

--- buildroot-external/package/skill-naptime/skill-naptime.mk
+++ buildroot-external/package/skill-naptime/skill-naptime.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SKILL_NAPTIME_VERSION = 7a178b1ac7dc438235d09aadeb289a0a19c6aa0a
-SKILL_NAPTIME_SITE = $(call github,OpenVoiceOS,skill-naptime,$(SKILL_NAPTIME_VERSION))
+SKILL_NAPTIME_SITE = $(call github,OpenVoiceOS,ovos-skill-naptime,$(SKILL_NAPTIME_VERSION))
 SKILL_NAPTIME_SETUP_TYPE = setuptools
 SKILL_NAPTIME_LICENSE_FILES = LICENSE
 
