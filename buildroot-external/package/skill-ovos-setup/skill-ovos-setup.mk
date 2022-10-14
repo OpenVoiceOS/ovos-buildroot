51651979d1815afb297e97decd91652c09a00156
j1nx <p.steenbergen@j1nx.nl>
2022-10-14 16:25:23 +0200

    Bump skill-ovos-setup

--- buildroot-external/package/skill-ovos-setup/skill-ovos-setup.mk
+++ buildroot-external/package/skill-ovos-setup/skill-ovos-setup.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_SETUP_VERSION = 038543831ec3a38a4d7701f99bca63e55eb80043
+SKILL_OVOS_SETUP_VERSION = 713404dbc9a4b594f09a292f7fd5b25f01aea850
 SKILL_OVOS_SETUP_SITE = $(call github,OpenVoiceOS,skill-ovos-setup,$(SKILL_OVOS_SETUP_VERSION))
 SKILL_OVOS_SETUP_SETUP_TYPE = setuptools
 SKILL_OVOS_SETUP_LICENSE_FILES = LICENSE
