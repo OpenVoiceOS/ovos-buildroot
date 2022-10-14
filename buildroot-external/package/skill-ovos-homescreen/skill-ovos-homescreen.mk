2fb34ab56ce9651b9f7466782fd26c691d8e0031
j1nx <p.steenbergen@j1nx.nl>
2022-10-14 15:59:59 +0200

    Bump and MYCROFT_LOOSE_REQUIREMENTS=true for skill-ovos-homescreen

--- buildroot-external/package/skill-ovos-homescreen/skill-ovos-homescreen.mk
+++ buildroot-external/package/skill-ovos-homescreen/skill-ovos-homescreen.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-SKILL_OVOS_HOMESCREEN_VERSION = 9f27f970745f5f87e1778e50f82f039edf1f225d
+SKILL_OVOS_HOMESCREEN_VERSION = e12f382af8468283763b5fdb31e3413dfa2b2a0b
 SKILL_OVOS_HOMESCREEN_SITE = $(call github,OpenVoiceOS,skill-ovos-homescreen,$(SKILL_OVOS_HOMESCREEN_VERSION))
 SKILL_OVOS_HOMESCREEN_SETUP_TYPE = setuptools
 SKILL_OVOS_HOMESCREEN_LICENSE_FILES = LICENSE
+SKILL_OVOS_HOMESCREEN_ENV = MYCROFT_LOOSE_REQUIREMENTS=true
 
 $(eval $(python-package))
