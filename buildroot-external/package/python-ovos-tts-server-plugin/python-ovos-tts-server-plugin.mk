fbf8f41d06397401dad1aa487685d118df40844a
j1nx <p.steenbergen@j1nx.nl>
2022-10-18 09:39:24 +0200

    Bump ovos-tts-server-plugin

--- buildroot-external/package/python-ovos-tts-server-plugin/python-ovos-tts-server-plugin.mk
+++ buildroot-external/package/python-ovos-tts-server-plugin/python-ovos-tts-server-plugin.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_OVOS_TTS_SERVER_PLUGIN_VERSION = 692d129030a3876b7e77f92a3a3a6285a4a5537a
+PYTHON_OVOS_TTS_SERVER_PLUGIN_VERSION = 43f44f64a1ced28f93dd6a633fcea2e4d00e5c2e
 PYTHON_OVOS_TTS_SERVER_PLUGIN_SITE = $(call github,OpenVoiceOS,ovos-tts-server-plugin,$(PYTHON_OVOS_TTS_SERVER_PLUGIN_VERSION))
 PYTHON_OVOS_TTS_SERVER_PLUGIN_SETUP_TYPE = setuptools
+PYTHON_OVOS_TTS_SERVER_PLUGIN_ENV = MYCROFT_LOOSE_REQUIREMENTS=true
 
 $(eval $(python-package))
