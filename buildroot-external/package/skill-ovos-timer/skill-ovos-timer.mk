47da128eeba57df91edb63fe0488fb1eadac68ba
j1nx <p.steenbergen@j1nx.nl>
2022-10-18 17:13:21 +0200

    Bump skill-ovos-timer

--- buildroot-external/package/skill-ovos-timer/skill-ovos-timer.mk
+++ buildroot-external/package/skill-ovos-timer/skill-ovos-timer.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-SKILL_OVOS_TIMER_VERSION = acc876308316f017bbcab21f77c4bae5b61c5ea7
+SKILL_OVOS_TIMER_VERSION = 7cedc9e8c3055bcc212fd5bb2b53b8383382c7e1
 SKILL_OVOS_TIMER_SITE = $(call github,OpenVoiceOS,skill-ovos-timer,$(SKILL_OVOS_TIMER_VERSION))
 SKILL_OVOS_TIMER_SETUP_TYPE = setuptools
 SKILL_OVOS_TIMER_LICENSE_FILES = LICENSE
+SKILL_OVOS_TIMER_ENV = MYCROFT_LOOSE_REQUIREMENTS=true
 
 $(eval $(python-package))
