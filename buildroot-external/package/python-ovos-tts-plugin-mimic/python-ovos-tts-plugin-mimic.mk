5b45206a874ebecd169c577ed6cfe6d20e7c7a97
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 12:15:16 +0200

    Bump ovos-tts-plugin-mimic

--- buildroot-external/package/python-ovos-tts-plugin-mimic/python-ovos-tts-plugin-mimic.mk
+++ buildroot-external/package/python-ovos-tts-plugin-mimic/python-ovos-tts-plugin-mimic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_TTS_PLUGIN_MIMIC_VERSION = dd4a3d0d9d45beed1f7387c918a0bbba0ab41904
+PYTHON_OVOS_TTS_PLUGIN_MIMIC_VERSION = efff5ea111d594cac8bafd257c0d03c5c9e7ac1f
 PYTHON_OVOS_TTS_PLUGIN_MIMIC_SITE = $(call github,OpenVoiceOS,ovos-tts-plugin-mimic,$(PYTHON_OVOS_TTS_PLUGIN_MIMIC_VERSION))
 PYTHON_OVOS_TTS_PLUGIN_MIMIC_SETUP_TYPE = setuptools
 PYTHON_OVOS_TTS_PLUGIN_MIMIC_ENV = MYCROFT_LOOSE_REQUIREMENTS=true
