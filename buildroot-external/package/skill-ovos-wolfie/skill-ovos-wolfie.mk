e3c7f23a000ddef947961c56243cb75e9dae058f
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 16:50:26 +0200

    Bump skill-ovos-wolfie

--- buildroot-external/package/skill-ovos-wolfie/skill-ovos-wolfie.mk
+++ buildroot-external/package/skill-ovos-wolfie/skill-ovos-wolfie.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_WOLFIE_VERSION = 6c2a82cdeedc5e963df35201f833e67dc8badfba
+SKILL_OVOS_WOLFIE_VERSION = 1e4af2fa0735f88d19fca917c7130a0995d17fc7
 SKILL_OVOS_WOLFIE_SITE = $(call github,OpenVoiceOS,skill-ovos-wolfie,$(SKILL_OVOS_WOLFIE_VERSION))
 SKILL_OVOS_WOLFIE_SETUP_TYPE = setuptools
 SKILL_OVOS_WOLFIE_LICENSE_FILES = LICENSE
