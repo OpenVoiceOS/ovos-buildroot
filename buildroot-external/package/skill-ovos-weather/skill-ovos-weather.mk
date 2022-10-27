d39b73807adc78c25dd1d22edeae3b59ebd4970b
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 15:14:23 +0200

    Bump skill-ovos-weather

--- buildroot-external/package/skill-ovos-weather/skill-ovos-weather.mk
+++ buildroot-external/package/skill-ovos-weather/skill-ovos-weather.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_WEATHER_VERSION = e275b90fc0738d4677586e207e39bbd84660e857
+SKILL_OVOS_WEATHER_VERSION = 133f659c58331a1322587532c57e6a23f6baa83d
 SKILL_OVOS_WEATHER_SITE = $(call github,OpenVoiceOS,skill-ovos-weather,$(SKILL_OVOS_WEATHER_VERSION))
 SKILL_OVOS_WEATHER_SETUP_TYPE = setuptools
 SKILL_OVOS_WEATHER_LICENSE_FILES = LICENSE
