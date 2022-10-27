cc0601e92b9b20eded6cf2a0cbe078522848b57c
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 17:33:34 +0200

    Bump skill-ovos-alarm

--- buildroot-external/package/skill-ovos-alarm/skill-ovos-alarm.mk
+++ buildroot-external/package/skill-ovos-alarm/skill-ovos-alarm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_ALARM_VERSION = a56428291b706f46add9828cb5f7f29ac41a08e1
+SKILL_OVOS_ALARM_VERSION = 6c14d41903227be490b5249ef464585b7694e4c1
 SKILL_OVOS_ALARM_SITE = $(call github,OpenVoiceOS,skill-ovos-alarm,$(SKILL_OVOS_ALARM_VERSION))
 SKILL_OVOS_ALARM_SETUP_TYPE = setuptools
 SKILL_OVOS_ALARM_LICENSE_FILES = LICENSE
