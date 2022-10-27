b2c200fdba1aa66f4be8d8cb3abe0970388d9409
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 17:30:54 +0200

    Bump skill-ovos-stop

--- buildroot-external/package/skill-ovos-stop/skill-ovos-stop.mk
+++ buildroot-external/package/skill-ovos-stop/skill-ovos-stop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_STOP_VERSION = 8c42181e3b1eac413dfb673cfcd686d4dc7b8a0c
+SKILL_OVOS_STOP_VERSION = cd940b2323533b64465a72b8935e322fce303719
 SKILL_OVOS_STOP_SITE = $(call github,OpenVoiceOS,skill-ovos-stop,$(SKILL_OVOS_STOP_VERSION))
 SKILL_OVOS_STOP_SETUP_TYPE = setuptools
 SKILL_OVOS_STOP_LICENSE_FILES = LICENSE
