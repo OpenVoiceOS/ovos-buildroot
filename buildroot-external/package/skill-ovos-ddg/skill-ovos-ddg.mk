61e7a009315b9dcfcb61855aeec7850aba68e6cf
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 16:58:01 +0200

    Bump skill-ovos-ddg

--- buildroot-external/package/skill-ovos-ddg/skill-ovos-ddg.mk
+++ buildroot-external/package/skill-ovos-ddg/skill-ovos-ddg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_DDG_VERSION = 6209146d4714dfa6a5588a9bcc10df16d5cbb373
+SKILL_OVOS_DDG_VERSION = 76dd835225ba69c4f6f91edc95c47d142829ba92
 SKILL_OVOS_DDG_SITE = $(call github,OpenVoiceOS,skill-ovos-ddg,$(SKILL_OVOS_DDG_VERSION))
 SKILL_OVOS_DDG_SETUP_TYPE = setuptools
 SKILL_OVOS_DDG_LICENSE_FILES = LICENSE
