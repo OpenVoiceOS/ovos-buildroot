52dae4375ee16958ff6c67d8ecadd07bd44629ff
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 10:58:07 +0200

    Bump skill-ovos-homescreen

--- buildroot-external/package/skill-ovos-homescreen/skill-ovos-homescreen.mk
+++ buildroot-external/package/skill-ovos-homescreen/skill-ovos-homescreen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_HOMESCREEN_VERSION = e12f382af8468283763b5fdb31e3413dfa2b2a0b
+SKILL_OVOS_HOMESCREEN_VERSION = 050aea49648201f21bef0b31782fe066c53fbf6f
 SKILL_OVOS_HOMESCREEN_SITE = $(call github,OpenVoiceOS,skill-ovos-homescreen,$(SKILL_OVOS_HOMESCREEN_VERSION))
 SKILL_OVOS_HOMESCREEN_SETUP_TYPE = setuptools
 SKILL_OVOS_HOMESCREEN_LICENSE_FILES = LICENSE
