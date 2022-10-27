74131da75a25d38b3637d5d05e0e902c6d6d4151
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 15:40:58 +0200

    Bump skill-ovos-notes

--- buildroot-external/package/skill-ovos-notes/skill-ovos-notes.mk
+++ buildroot-external/package/skill-ovos-notes/skill-ovos-notes.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_NOTES_VERSION = d4522f4bb544fe98324133781d6af82eaa25a520
+SKILL_OVOS_NOTES_VERSION = adcfc50268846852f16cb9205a1dae26055151c4
 SKILL_OVOS_NOTES_SITE = $(call github,OpenVoiceOS,skill-ovos-notes,$(SKILL_OVOS_NOTES_VERSION))
 SKILL_OVOS_NOTES_SETUP_TYPE = setuptools
 SKILL_OVOS_NOTES_LICENSE_FILES = LICENSE
