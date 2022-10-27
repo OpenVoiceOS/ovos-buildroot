85b7edbeb9d390111d690c6d9f5499a2bebfc428
j1nx <p.steenbergen@j1nx.nl>
2022-10-27 17:28:07 +0200

    Bump skill-ovos-naptime

--- buildroot-external/package/skill-ovos-naptime/skill-ovos-naptime.mk
+++ buildroot-external/package/skill-ovos-naptime/skill-ovos-naptime.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_NAPTIME_VERSION = b78703d45f9035f3741d849928be30a67b178d51
+SKILL_OVOS_NAPTIME_VERSION = b43ebb000da929833702d42dea7711ef9861e2ff
 SKILL_OVOS_NAPTIME_SITE = $(call github,OpenVoiceOS,skill-ovos-naptime,$(SKILL_OVOS_NAPTIME_VERSION))
 SKILL_OVOS_NAPTIME_SETUP_TYPE = setuptools
 SKILL_OVOS_NAPTIME_LICENSE_FILES = LICENSE
