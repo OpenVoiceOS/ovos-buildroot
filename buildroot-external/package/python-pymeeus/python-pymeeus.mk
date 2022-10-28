86831c610674626e18e6e6842bfafae5f5328be6
j1nx <p.steenbergen@j1nx.nl>
2022-10-28 19:52:08 +0200

    Bump python-pymeeus

--- buildroot-external/package/python-pymeeus/python-pymeeus.mk
+++ buildroot-external/package/python-pymeeus/python-pymeeus.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYMEEUS_VERSION = 0.3.7
+PYTHON_PYMEEUS_VERSION = 0.5.11
 PYTHON_PYMEEUS_SOURCE = PyMeeus-$(PYTHON_PYMEEUS_VERSION).tar.gz
-PYTHON_PYMEEUS_SITE = https://files.pythonhosted.org/packages/2c/30/47957d487fff94083bdd17247734c165f12b5ac39a3abd1aa476a93eea6e
+PYTHON_PYMEEUS_SITE = https://files.pythonhosted.org/packages/c7/ff/0f0a0becf088281c6bc6c75b7d7c03a2481d486ef6cc7c8899bbcab0a88d
 PYTHON_PYMEEUS_SETUP_TYPE = setuptools
-PYTHON_PYMEEUS_LICENSE = GPL-3.0
+PYTHON_PYMEEUS_LICENSE = GNU Lesser General Public License v3 (LGPLv3)
 PYTHON_PYMEEUS_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
