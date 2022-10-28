f8dd23d69892309fca49b62e223cb21d4bc0e939
j1nx <p.steenbergen@j1nx.nl>
2022-10-28 20:19:01 +0200

    Bump ovos-stt-plugin-selene

--- buildroot-external/package/python-ovos-stt-plugin-selene/python-ovos-stt-plugin-selene.mk
+++ buildroot-external/package/python-ovos-stt-plugin-selene/python-ovos-stt-plugin-selene.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_STT_PLUGIN_SELENE_VERSION = d96863f34fec89747932a7e4e2c36ce88534976f
+PYTHON_OVOS_STT_PLUGIN_SELENE_VERSION = 49e5f16da293c83c54d4d71e9d836f44fe469634
 PYTHON_OVOS_STT_PLUGIN_SELENE_SITE = $(call github,OpenVoiceOS,ovos-stt-plugin-selene,$(PYTHON_OVOS_STT_PLUGIN_SELENE_VERSION))
 PYTHON_OVOS_STT_PLUGIN_SELENE_SETUP_TYPE = setuptools
 
