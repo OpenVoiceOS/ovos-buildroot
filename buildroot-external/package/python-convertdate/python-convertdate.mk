d2a4344203d77ca530892aa1dc99b711f3deb267
j1nx <p.steenbergen@j1nx.nl>
2022-10-28 19:46:55 +0200

    Bump python-convertdate

--- buildroot-external/package/python-convertdate/python-convertdate.mk
+++ buildroot-external/package/python-convertdate/python-convertdate.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CONVERTDATE_VERSION = 2.2.0
+PYTHON_CONVERTDATE_VERSION = 2.4.0
 PYTHON_CONVERTDATE_SOURCE = convertdate-$(PYTHON_CONVERTDATE_VERSION).tar.gz
-PYTHON_CONVERTDATE_SITE = https://files.pythonhosted.org/packages/92/c1/1125eba52ce9bccf783f0640eaad39ffa6e4271dcf37d19438c2ef115233
+PYTHON_CONVERTDATE_SITE = https://files.pythonhosted.org/packages/04/3d/04148ceb732dfb6f10e9b89fa5915080a91e27fe28fd982c259bc4d29ced
 PYTHON_CONVERTDATE_SETUP_TYPE = setuptools
 PYTHON_CONVERTDATE_LICENSE = MIT
 PYTHON_CONVERTDATE_LICENSE_FILES = LICENSE
