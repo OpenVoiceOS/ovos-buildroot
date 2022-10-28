41b5b9b01c6bfd7c98c3c047bf7156ddd70a85ec
j1nx <p.steenbergen@j1nx.nl>
2022-10-28 19:41:35 +0200

    Bump python-holidays

--- buildroot-external/package/python-holidays/python-holidays.mk
+++ buildroot-external/package/python-holidays/python-holidays.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HOLIDAYS_VERSION = 0.10.4
+PYTHON_HOLIDAYS_VERSION = 0.16
 PYTHON_HOLIDAYS_SOURCE = holidays-$(PYTHON_HOLIDAYS_VERSION).tar.gz
-PYTHON_HOLIDAYS_SITE = https://files.pythonhosted.org/packages/cc/14/a8a30bae14091984acd262526a258b1890c3ada0e5ae0da747e2b8f2b77d
+PYTHON_HOLIDAYS_SITE = https://files.pythonhosted.org/packages/e9/64/d52bb3e440a2780af0ad8721526b3804f721df045357efd0778372e20489
 PYTHON_HOLIDAYS_SETUP_TYPE = setuptools
 PYTHON_HOLIDAYS_LICENSE = MIT
 PYTHON_HOLIDAYS_LICENSE_FILES = LICENSE
