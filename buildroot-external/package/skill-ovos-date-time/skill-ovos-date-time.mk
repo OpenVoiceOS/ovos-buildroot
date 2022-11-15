f5781552ab9099226bfec70bdaf56c554a81de38
j1nx <p.steenbergen@j1nx.nl>
2022-11-15 09:17:19 +0100

    Bump skill-ovos-date-time

--- buildroot-external/package/skill-ovos-date-time/skill-ovos-date-time.mk
+++ buildroot-external/package/skill-ovos-date-time/skill-ovos-date-time.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_DATE_TIME_VERSION = 824b271a6722e016a8ffc80111b595138abb91ff
+SKILL_OVOS_DATE_TIME_VERSION = 0c3c65d291966df82b0f50443abb98ea8bc3d93d
 SKILL_OVOS_DATE_TIME_SITE = $(call github,OpenVoiceOS,skill-ovos-date-time,$(SKILL_OVOS_DATE_TIME_VERSION))
 SKILL_OVOS_DATE_TIME_SETUP_TYPE = setuptools
 SKILL_OVOS_DATE_TIME_LICENSE_FILES = LICENSE
