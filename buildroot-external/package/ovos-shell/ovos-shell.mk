0d5d4079ed9a6f08ab2acab45407d24a57819aff
j1nx <p.steenbergen@j1nx.nl>
2022-11-17 20:06:15 +0100

    Bump ovos-shell

--- buildroot-external/package/ovos-shell/ovos-shell.mk
+++ buildroot-external/package/ovos-shell/ovos-shell.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OVOS_SHELL_VERSION = 0c2f950c7a88c34bdf6cd25d14f4765e11c1c010
+OVOS_SHELL_VERSION = 293706bf1bbcb0774872c514575ff878f31d096d
 OVOS_SHELL_SITE = $(call github,OpenVoiceOS,ovos-shell,$(OVOS_SHELL_VERSION))
 OVOS_SHELL_LICENSE = Apache License 2.0
 
