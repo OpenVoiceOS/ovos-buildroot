3c5ad11f8b43e30419bc02fd6db7933699433d89
j1nx <p.steenbergen@j1nx.nl>
2022-11-21 12:20:55 +0100

    Bump ovos-ww-plugin-vosk

--- buildroot-external/package/python-ovos-ww-plugin-vosk/python-ovos-ww-plugin-vosk.mk
+++ buildroot-external/package/python-ovos-ww-plugin-vosk/python-ovos-ww-plugin-vosk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_WW_PLUGIN_VOSK_VERSION = 1ee4917e19d8f39f9944c11f3f596a5e0cf2ff58
+PYTHON_OVOS_WW_PLUGIN_VOSK_VERSION = b5fbf1a553f97c27df9a0cfe54465aeed7b6105c
 PYTHON_OVOS_WW_PLUGIN_VOSK_SITE = $(call github,OpenVoiceOS,ovos-ww-plugin-vosk,$(PYTHON_OVOS_WW_PLUGIN_VOSK_VERSION))
 PYTHON_OVOS_WW_PLUGIN_VOSK_SETUP_TYPE = setuptools
 PYTHON_OVOS_WW_PLUGIN_VOSK_LICENSE_FILES = LICENSE
