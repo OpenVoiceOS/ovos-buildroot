b6e188c67d641d9a5642078a01a31d6b874a3105
j1nx <p.steenbergen@j1nx.nl>
2022-12-21 16:01:10 +0100

    [WIP] Start of DeepFilterNet ladspa plugin for denoise mic

---
+++ buildroot-external/package/deepfilternet-ladspa/deepfilternet-ladspa.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# deepfilternet-ladspa
+#
+################################################################################
+
+DEEPFILTERNET_LADSPA_VERSION = 27b5f07be6caba1abec7907fcca89ba906732800
+DEEPFILTERNET_LADSPA_SITE = $(call github,Rikorose,DeepFilterNet,$(DEEPFILTERNET_LADSPA_VERSION))
+
+DEEPFILTERNET_LADSPA_DEPENDENCIES = host-rustc host-pkgconf
+
+DEEPFILTERNET_LADSPA_CARGO_ENV = PKG_CONFIG_ALLOW_CROSS=1
+
+DEEPFILTERNET_LADSPA_BIN_DIR = target/$(RUSTC_TARGET_NAME)/release
+
+#define DEEPFILTERNET_LADSPA_INSTALL_TARGET_CMDS
+#	$(INSTALL) -D -m 0755 $(@D)/$(DEEPFILTERNET_LADSPA_BIN_DIR)/DEEPFILTERNET_LADSPA \
+#		$(TARGET_DIR)/usr/bin/DEEPFILTERNET_LADSPA
+#	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/DEEPFILTERNET_LADSPA/DEEPFILTERNET_LADSPA.conf \
+#		$(TARGET_DIR)/etc/DEEPFILTERNET_LADSPA.conf
+#endef
+
+$(eval $(cargo-package))
