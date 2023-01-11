d606b709200f996b0608b50844e4dc9e31deffd1
j1nx <p.steenbergen@j1nx.nl>
2023-01-11 13:59:21 +0100

    Bump skill-ovos-news

--- buildroot-external/package/skill-ovos-news/skill-ovos-news.mk
+++ buildroot-external/package/skill-ovos-news/skill-ovos-news.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKILL_OVOS_NEWS_VERSION = ff730b234edb5bc4e6b7cc0fb17e0a55f60cf474
+SKILL_OVOS_NEWS_VERSION = 392d32ebea4cc9cc3ba3371c1fe63c72426d0070
 SKILL_OVOS_NEWS_SITE = $(call github,OpenVoiceOS,skill-ovos-news,$(SKILL_OVOS_NEWS_VERSION))
 SKILL_OVOS_NEWS_SETUP_TYPE = setuptools
 SKILL_OVOS_NEWS_LICENSE_FILES = LICENSE
