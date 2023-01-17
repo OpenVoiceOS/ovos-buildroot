c630fb1ee03355af2db790b8b4cc1b1e169e1691
j1nx <p.steenbergen@j1nx.nl>
2023-01-17 10:19:18 +0100

    Bump whispercpp and build the example tools.

--- buildroot-external/package/whispercpp/whispercpp.mk
+++ buildroot-external/package/whispercpp/whispercpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WHISPERCPP_VERSION = aa6adda26e1ee9843dddb013890e3312bee52cfe
+WHISPERCPP_VERSION = 8738427dd60bda894df1ff3c12317cca2e960016
 WHISPERCPP_SITE = $(call github,ggerganov,whisper.cpp,$(WHISPERCPP_VERSION))
 WHISPERCPP_LICENSE = Apache License 2.0
 
@@ -14,6 +14,17 @@ WHISPERCPP_SUPPORTS_IN_SOURCE_BUILD = NO
 
 WHISPERCPP_CONF_OPTS = \
 	-DBUILD_SHARED_LIBS_DEFAULT=ON \
-	-DWHISPER_SUPPORT_OPENBLAS=ON
+	-DWHISPER_SUPPORT_OPENBLAS=ON \
+	-DWHISPER_BUILD_TESTS=ON \
+	-DWHISPER_BUILD_EXAMPLES=ON \
+	-DWHISPER_SUPPORT_SDL2=ON
+
+WHISPERCPP_POST_INSTALL_TARGET_HOOKS = WHISPERCPP_INSTALL_BINS
+
+define WHISPERCPP_INSTALL_BINS
+	mkdir -p $(TARGET_DIR)/usr/bin/whispercpp
+	$(INSTALL) -D -m 755 $(@D)/buildroot-build/bin/* \
+		$(TARGET_DIR)/usr/bin/whispercpp/
+endef
 
 $(eval $(cmake-package))
