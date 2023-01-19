53bae2b9e06c3a1d3088de227e6ad3497b72f86e
j1nx <p.steenbergen@j1nx.nl>
2023-01-19 15:30:03 +0100

    Bump python-scipy

--- buildroot-external/package/python-scipy/python-scipy.mk
+++ buildroot-external/package/python-scipy/python-scipy.mk
@@ -4,31 +4,31 @@
 #
 ################################################################################
 
-PYTHON_SCIPY_VERSION = 1.7.3
-PYTHON_SCIPY_SOURCE = scipy-$(PYTHON_SCIPY_VERSION).tar.xz
+PYTHON_SCIPY_VERSION = 1.9.1
+PYTHON_SCIPY_SOURCE = scipy-$(PYTHON_SCIPY_VERSION).tar.gz
 PYTHON_SCIPY_SITE = https://github.com/scipy/scipy/releases/download/v$(PYTHON_SCIPY_VERSION)
 PYTHON_SCIPY_LICENSE = \
 	BSD-3-Clause, \
 	BSD-2-Clause, \
 	BSD, \
 	BSD-Style, \
-	Apache-2.0, \
-	MIT
+	MIT, \
+	Qhull
 PYTHON_SCIPY_LICENSE_FILES = \
 	LICENSE.txt \
 	scipy/linalg/src/lapack_deprecations/LICENSE \
 	scipy/ndimage/LICENSE.txt \
 	scipy/optimize/tnc/LICENSE \
-	scipy/sparse/linalg/dsolve/SuperLU/License.txt \
-	scipy/sparse/linalg/eigen/arpack/ARPACK/COPYING \
+	scipy/sparse/linalg/_dsolve/SuperLU/License.txt \
+	scipy/sparse/linalg/_eigen/arpack/ARPACK/COPYING \
 	scipy/spatial/qhull_src/COPYING.txt
+PYTHON_SCIPY_CPE_ID_VENDOR = scipy
+PYTHON_SCIPY_CPE_ID_PRODUCT = scipy
 PYTHON_SCIPY_DEPENDENCIES += \
-	host-meson \
 	host-python-numpy \
 	host-python-pythran \
 	zlib \
 	lapack \
-	openblas \
 	python-numpy \
 	python-pybind
 PYTHON_SCIPY_INSTALL_STAGING = YES
@@ -36,22 +36,26 @@ PYTHON_SCIPY_INSTALL_STAGING = YES
 PYTHON_SCIPY_SETUP_TYPE = setuptools
 PYTHON_SCIPY_BUILD_OPTS = config_fc --fcompiler=gnu95
 
-# -lnpymath npymath.ini localization
-PYTHON_SCIPY_NPY_PKG_CONFIG_PATH = \
-	$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/core/lib/npy-pkg-config
-
+PYTHON_SCIPY_CFLAGS = \
+	-I$(STAGING_DIR)/usr/include/python$(PYTHON3_VERSION_MAJOR)
 PYTHON_SCIPY_LDFLAGS = $(TARGET_LDFLAGS) -shared \
 	-L$(PYTHON3_PATH)/site-packages/numpy/core/lib
 # -lnpyrandom localization
 PYTHON_SCIPY_LDFLAGS += \
 	-L$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/random/lib
 
+# scipy can use C++11 atomics when available, so we need to link with
+# libatomic for the architectures that need libatomic.
+ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
+PYTHON_SCIPY_LDFLAGS += -latomic
+endif
+
 PYTHON_SCIPY_ENV = \
 	F90=$(TARGET_FC) \
-	NPY_PKG_CONFIG_PATH=$(PYTHON_SCIPY_NPY_PKG_CONFIG_PATH) \
+	CFLAGS="$(PYTHON_SCIPY_CFLAGS)" \
 	LDFLAGS="$(PYTHON_SCIPY_LDFLAGS)"
 
-# Provide system configuration options to numpy distutils extenions, telling
+# Provide system configuration options to numpy distutils extensions, telling
 # to find all include files and libraries in staging directory.
 define PYTHON_SCIPY_CONFIGURE_CMDS
 	-rm -f $(@D)/site.cfg
