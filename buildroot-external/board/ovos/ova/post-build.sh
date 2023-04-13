3108dda9fe083c3c86eb36d3b33f8979ce63429d
j1nx <p.steenbergen@j1nx.nl>
2023-04-13 13:53:47 +0200

    Initial start of x86_64 support

---
+++ buildroot-external/board/ovos/ova/post-build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -u
+set -e
+
+BOARD_DIR="$(dirname $0)"
+
+. "${BR2_EXTERNAL_OPENVOICEOS_PATH}/meta"
+. "${BOARD_DIR}/meta"
+
+# Write os-release
+{
+    echo "NAME=\"${OVOS_NAME}\""
+    echo "VERSION=\"${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD} ${BOARD_NAME}\""
+    echo "ID=${OVOS_ID}"
+    echo "VERSION_ID=${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD}"
+    echo "PRETTY_NAME=\"${OVOS_NAME} ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD} ${BOARD_NAME}\""
+    echo "CPE_NAME=cpe:2.3:o:openvoiceos:${OVOS_ID}:${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD}:*:${DEPLOYMENT}:*:*:*:${BOARD_ID}:*"
+    echo "HOME_URL=https://github.com/OpenVoiceOS/OpenVoiceOS"
+    echo "DOCUMENTATION_URL=https://github.com/OpenVoiceOS/OpenVoiceOS/tree/develop/documentation"
+    echo "SUPPORT_URL=https://github.com/OpenVoiceOS/OpenVoiceOS/issues"
+    echo "VARIANT=\"${OVOS_NAME} - Mycroft Edition\""
+    echo "VARIANT_ID=${OVOS_ID}-${BOARD_ID}-mycroft"
+} > "${TARGET_DIR}/usr/lib/os-release"
+
+# Write machine-info
+{
+    echo "CHASSIS=${CHASSIS}"
+    echo "DEPLOYMENT=${DEPLOYMENT}"
+} > "${TARGET_DIR}/etc/machine-info"
+
+cp -f ../buildroot-external/board/ovos/ova/grub-efi.cfg ${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg
