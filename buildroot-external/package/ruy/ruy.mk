cb23c45f9285b2b88d37ffdc6145d807e3b75633
j1nx <p.steenbergen@j1nx.nl>
2023-04-25 13:16:50 +0200

    Allow tensorflow-lite to be build for both aarch64 and x86-64

--- buildroot-external/package/ruy/ruy.mk
+++ buildroot-external/package/ruy/ruy.mk
@@ -4,16 +4,14 @@
 #
 ################################################################################
 
-RUY_VERSION = 3168a5c8f4c447fd8cea94078121ee2e2cd87df0
+RUY_VERSION = 21a85fef159f9942f636a43b14c64b481c2a05b2
 RUY_SITE = $(call github,google,ruy,$(RUY_VERSION))
 RUY_LICENSE = Apache-2
 RUY_LICENSE_FILES = LICENSE
+RUY_INSTALL_STAGING = YES
 RUY_DEPENDENCIES = cpuinfo
-RUY_SUPPORTS_IN_SOURCE_BUILD = NO
 RUY_CONF_OPTS = -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
 		-DRUY_FIND_CPUINFO=ON \
 		-DRUY_MINIMAL_BUILD=ON
-
-RUY_INSTALL_STAGING = YES
 
 $(eval $(cmake-package))
--- buildroot-external/package/tensorflow-lite/tensorflow-lite.mk
+++ buildroot-external/package/tensorflow-lite/tensorflow-lite.mk
@@ -4,29 +4,24 @@
 #
 ################################################################################
 
-TENSORFLOW_LITE_VERSION = v2.11.0
-TENSORFLOW_LITE_SITE = $(call github,tensorflow,tensorflow,$(TENSORFLOW_LITE_VERSION))
-TENSORFLOW_LITE_SUBDIR = tensorflow/lite
+TENSORFLOW_LITE_VERSION = 2.11.0
+TENSORFLOW_LITE_SITE =  $(call github,tensorflow,tensorflow,v$(TENSORFLOW_LITE_VERSION))
+TENSORFLOW_LITE_INSTALL_STAGING = YES
 TENSORFLOW_LITE_LICENSE = Apache-2.0
 TENSORFLOW_LITE_LICENSE_FILES = LICENSE
-
-TENSORFLOW_LITE_INSTALL_STAGING = YES
+TENSORFLOW_LITE_SUBDIR = tensorflow/lite
 TENSORFLOW_LITE_SUPPORTS_IN_SOURCE_BUILD = NO
 TENSORFLOW_LITE_DEPENDENCIES += \
 	host-pkgconf \
-        host-flatbuffers \
-        cpuinfo \
-        eigen \
-        farmhash \
-        fft2d \
-        flatbuffers \
-        gemmlowp \
-        libabseil-cpp \
-        neon2sse \
-        ruy \
-	xnnpack \
-	opencl_headers \
-	vulkan-headers
+	host-flatbuffers \
+	cpuinfo \
+	eigen \
+	farmhash \
+	fft2d \
+	flatbuffers \
+	gemmlowp \
+	libabseil-cpp \
+	neon-2-sse
 
 TENSORFLOW_LITE_CONF_OPTS = \
 	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -funsafe-math-optimizations \
@@ -39,25 +34,36 @@ TENSORFLOW_LITE_CONF_OPTS = \
 		-I$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/core/include \
 		-I$(STAGING_DIR)/usr/include/pybind11 \
 		-I$(STAGING_DIR)/usr/include/gemmlowp" \
+	-Dabsl_DIR=$(STAGING_DIR)/usr/lib/cmake/absl \
 	-DBUILD_SHARED_LIBS=ON \
 	-DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON \
-        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-	-Dabsl_DIR=$(STAGING_DIR)/usr/lib/cmake/absl \
-        -DEigen3_DIR=$(STAGING_DIR)/usr/share/eigen3/cmake \
-        -DFARMHASH_SOURCE_DIR=$(FARMHASH_DIR) \
-        -Dfarmhash_DIR=$(STAGING_DIR)/usr/lib \
-        -DFETCHCONTENT_FULLY_DISCONNECTED=ON \
-        -DFETCHCONTENT_QUIET=OFF \
-        -DFFT2D_SOURCE_DIR=$(STAGING_DIR)/usr/include/fft2d \
-        -DFlatBuffers_DIR=$(STAGING_DIR)/usr/lib/cmake/flatbuffers \
-        -DNEON_2_SSE_DIR=$(STAGING_DIR)/usr/lib/cmake/NEON_2_SSE \
-        -DTFLITE_ENABLE_EXTERNAL_DELEGATE=ON \
-        -DTFLITE_ENABLE_GPU=OFF \
-        -DTFLITE_ENABLE_INSTALL=ON \
-        -DTFLITE_ENABLE_MMAP=ON \
-        -DTFLITE_ENABLE_NNAPI=OFF \
-        -DTFLITE_ENABLE_RUY=ON \
-        -DTFLITE_ENABLE_XNNPACK=ON
+	-DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+	-DEigen3_DIR=$(STAGING_DIR)/usr/share/eigen3/cmake \
+	-DFETCHCONTENT_FULLY_DISCONNECTED=ON \
+	-DFETCHCONTENT_QUIET=OFF \
+	-DFFT2D_SOURCE_DIR=$(STAGING_DIR)/usr/include/fft2d \
+	-DFlatBuffers_DIR=$(STAGING_DIR)/usr/lib/cmake/flatbuffers \
+	-DNEON_2_SSE_DIR=$(STAGING_DIR)/usr/lib/cmake/NEON_2_SSE \
+	-DSYSTEM_FARMHASH=ON \
+	-DTFLITE_ENABLE_EXTERNAL_DELEGATE=ON \
+	-DTFLITE_ENABLE_GPU=OFF \
+	-DTFLITE_ENABLE_INSTALL=ON \
+	-DTFLITE_ENABLE_MMAP=ON \
+	-DTFLITE_ENABLE_NNAPI=OFF
+
+ifeq ($(BR2_PACKAGE_RUY),y)
+TENSORFLOW_LITE_DEPENDENCIES += ruy
+TENSORFLOW_LITE_CONF_OPTS += -DTFLITE_ENABLE_RUY=ON
+else
+TENSORFLOW_LITE_CONF_OPTS += -DTFLITE_ENABLE_RUY=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_XNNPACK),y)
+TENSORFLOW_LITE_DEPENDENCIES += xnnpack
+TENSORFLOW_LITE_CONF_OPTS += -DTFLITE_ENABLE_XNNPACK=ON -Dxnnpack_POPULATED=ON
+else
+TENSORFLOW_LITE_CONF_OPTS += -DTFLITE_ENABLE_XNNPACK=OFF
+endif
 
 TENSORFLOW_LITE_MAKE_OPTS += _pywrap_tensorflow_interpreter_wrapper benchmark_model
 
@@ -74,7 +80,7 @@ endef
 define TENSORFLOW_LITE_INSTALL_TFLITE_RUNTIME
 
 	mkdir -p $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime
-	mkdir -p $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-2.11.0-py3.10.egg-info
+	mkdir -p $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-${TENSORFLOW_LITE_VERSION}-py$(PYTHON3_VERSION_MAJOR).egg-info
 
 	$(INSTALL) -D -m 755 $(@D)/$(TENSORFLOW_LITE_SUBDIR)/buildroot-build/_pywrap_tensorflow_interpreter_wrapper.so \
 		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime/
@@ -95,19 +101,19 @@ define TENSORFLOW_LITE_INSTALL_TFLITE_RUNTIME
 		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime/
 
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/tensorflow-lite/tflite_runtime/dependency_links.txt \
-		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-2.11.0-py3.10.egg-info/
+		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-${TENSORFLOW_LITE_VERSION}-py$(PYTHON3_VERSION_MAJOR).egg-info/
 
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/tensorflow-lite/tflite_runtime/PKG-INFO \
-		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-2.11.0-py3.10.egg-info/
+		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-${TENSORFLOW_LITE_VERSION}-py$(PYTHON3_VERSION_MAJOR).egg-info/
 
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/tensorflow-lite/tflite_runtime/requires.txt \
-		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-2.11.0-py3.10.egg-info/
+		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-${TENSORFLOW_LITE_VERSION}-py$(PYTHON3_VERSION_MAJOR).egg-info/
 
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/tensorflow-lite/tflite_runtime/SOURCES.txt \
-		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-2.11.0-py3.10.egg-info/
+		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-${TENSORFLOW_LITE_VERSION}-py$(PYTHON3_VERSION_MAJOR).egg-info/
 
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/tensorflow-lite/tflite_runtime/top_level.txt \
-		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-2.11.0-py3.10.egg-info/
+		$(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/tflite_runtime-${TENSORFLOW_LITE_VERSION}-py$(PYTHON3_VERSION_MAJOR).egg-info/
 
 endef
 
