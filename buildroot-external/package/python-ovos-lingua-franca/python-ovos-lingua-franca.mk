da256fef7fe1c08d479ac8b531102805fd0a4773
j1nx <p.steenbergen@j1nx.nl>
2023-05-06 09:38:21 +0200

    Bump ovos-lingua-franca

--- buildroot-external/package/python-ovos-lingua-franca/python-ovos-lingua-franca.mk
+++ buildroot-external/package/python-ovos-lingua-franca/python-ovos-lingua-franca.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_LINGUA_FRANCA_VERSION = 86a8f117ce7ca56a9ccf9cea27bcd5e63a7b54c8
+PYTHON_OVOS_LINGUA_FRANCA_VERSION = 0c4ead9b3ad396fc68a5b5c6465678b567c6c01f
 PYTHON_OVOS_LINGUA_FRANCA_SITE = $(call github,OpenVoiceOS,ovos-lingua-franca,$(PYTHON_OVOS_LINGUA_FRANCA_VERSION))
 PYTHON_OVOS_LINGUA_FRANCA_SETUP_TYPE = setuptools
 PYTHON_OVOS_LINGUA_FRANCA_LICENSE = Apache-2.0
