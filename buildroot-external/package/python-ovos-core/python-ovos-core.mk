73d7ee6d34cd7e5acc92c25bc71a7c55d0d0941a
j1nx <p.steenbergen@j1nx.nl>
2023-05-06 15:26:58 +0200

    Bump ovos-core

--- buildroot-external/package/python-ovos-core/python-ovos-core.mk
+++ buildroot-external/package/python-ovos-core/python-ovos-core.mk
@@ -4,16 +4,10 @@
 #
 ################################################################################
 
-PYTHON_OVOS_CORE_VERSION = 8e54d544c40d4e4a227d886bdac933b0498cc7d2
+PYTHON_OVOS_CORE_VERSION = 79185d366fd3ad89a0a9d7f180ec5f6ec1f3d4b7
 PYTHON_OVOS_CORE_SITE = $(call github,OpenVoiceOS,ovos-core,$(PYTHON_OVOS_CORE_VERSION))
 PYTHON_OVOS_CORE_SETUP_TYPE = setuptools
 PYTHON_OVOS_CORE_LICENSE_FILES = LICENSE
 PYTHON_OVOS_CORE_ENV = MYCROFT_LOOSE_REQUIREMENTS=true
-
-PYTHON_OVOS_CORE_PRE_CONFIGURE_HOOKS = PYTHON_OVOS_CORE_REQUIREMENTS
-
-define PYTHON_OVOS_CORE_REQUIREMENTS
-	cp $(@D)/requirements/requirements.txt $(@D)/requirements/minimal.txt
-endef
 
 $(eval $(python-package))
