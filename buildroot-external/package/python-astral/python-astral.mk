a6ce3ff7a49f7d67239d20fab8003d2adffe86a3
j1nx <p.steenbergen@j1nx.nl>
2023-05-07 13:35:03 +0200

    Bump astral

--- buildroot-external/package/python-astral/python-astral.mk
+++ buildroot-external/package/python-astral/python-astral.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-PYTHON_ASTRAL_VERSION = 1.4
-PYTHON_ASTRAL_SOURCE = astral-$(PYTHON_ASTRAL_VERSION).zip
-PYTHON_ASTRAL_SITE = https://files.pythonhosted.org/packages/a4/d6/c309f266677372964c6002a56a7bec2a90875338a71325e20b24d6d1e187
+PYTHON_ASTRAL_VERSION = 3.2
+PYTHON_ASTRAL_SOURCE = astral-$(PYTHON_ASTRAL_VERSION).tar.gz
+PYTHON_ASTRAL_SITE = https://files.pythonhosted.org/packages/04/d1/1adbf06a38dc339e41a1666f6c7135924594c20fd46e060fb263248c564d
 PYTHON_ASTRAL_DEPENDENCIES = host-python-pytz
 PYTHON_ASTRAL_SETUP_TYPE = setuptools
 PYTHON_ASTRAL_LICENSE = Apache-2.0
 PYTHON_ASTRAL_LICENSE_FILES = LICENSE
-
-define PYTHON_ASTRAL_EXTRACT_CMDS
-        $(UNZIP) -d $(@D) $(DL_DIR)/python-astral/$(PYTHON_ASTRAL_SOURCE)
-        mv $(@D)/astral-$(PYTHON_ASTRAL_VERSION)/* $(@D)
-        $(RM) -r $(@D)/astral-$(PYTHON_ASTRAL_VERSION)
-endef
 
 $(eval $(python-package))
