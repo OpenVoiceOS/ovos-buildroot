225c0ca6491a3a669f66620e64e3ef7da5af83b1
j1nx <p.steenbergen@j1nx.nl>
2023-05-09 19:10:46 +0200

    Bump ovos-ocp-m3u-plugin

--- buildroot-external/package/python-ovos-ocp-m3u-plugin/python-ovos-ocp-m3u-plugin.mk
+++ buildroot-external/package/python-ovos-ocp-m3u-plugin/python-ovos-ocp-m3u-plugin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_OCP_M3U_PLUGIN_VERSION = aec50ace75e488ea0316ef9a6b024fe7430267fb
+PYTHON_OVOS_OCP_M3U_PLUGIN_VERSION = d2571ac1c2870501f0940dc3dcdb35b402300e9e
 PYTHON_OVOS_OCP_M3U_PLUGIN_SITE = $(call github,OpenVoiceOS,ovos-ocp-m3u-plugin,$(PYTHON_OVOS_OCP_M3U_PLUGIN_VERSION))
 PYTHON_OVOS_OCP_M3U_PLUGIN_SETUP_TYPE = setuptools
 PYTHON_OVOS_OCP_M3U_PLUGIN_LICENSE_M3U = LICENSE
