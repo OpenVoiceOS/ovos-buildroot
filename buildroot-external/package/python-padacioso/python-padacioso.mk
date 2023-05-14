d9659cad8bbb467d210717d2f71c7ab8801a3847
j1nx <p.steenbergen@j1nx.nl>
2023-05-14 10:21:02 +0200

    Bump padacioso

--- buildroot-external/package/python-padacioso/python-padacioso.mk
+++ buildroot-external/package/python-padacioso/python-padacioso.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PADACIOSO_VERSION = 8888ffe95731edafad02e9603559abd24bb363a4
+PYTHON_PADACIOSO_VERSION = 02da04c52ab0eaf8e15e7ea1ff9a83bfc51533f6
 PYTHON_PADACIOSO_SITE = $(call github,OpenVoiceOS,padacioso,$(PYTHON_PADACIOSO_VERSION))
 PYTHON_PADACIOSO_SETUP_TYPE = setuptools
 PYTHON_PADACIOSO_LICENSE = apache-2.0
