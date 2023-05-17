bb83433d0160dc1f3055afdd72725b0933902487
j1nx <p.steenbergen@j1nx.nl>
2023-05-17 09:48:57 +0200

    Bump ovos-listener

--- buildroot-external/package/python-ovos-listener/python-ovos-listener.mk
+++ buildroot-external/package/python-ovos-listener/python-ovos-listener.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_LISTENER_VERSION = 768cd42d50ff7439c359d07a2dbba085a1e8b657
+PYTHON_OVOS_LISTENER_VERSION = 9fa3f8667125d677316e21e4c37d1b9dae2053d2
 PYTHON_OVOS_LISTENER_SITE = $(call github,OpenVoiceOS,ovos-listener,$(PYTHON_OVOS_LISTENER_VERSION))
 PYTHON_OVOS_LISTENER_SETUP_TYPE = setuptools
 PYTHON_OVOS_LISTENER_LICENSE_FILES = LICENSE
