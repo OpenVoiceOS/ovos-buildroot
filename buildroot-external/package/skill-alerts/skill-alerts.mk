8ef0d4297bb30b088e32af9dd990325c68f4ea54
j1nx <p.steenbergen@j1nx.nl>
2023-05-17 09:21:52 +0200

    Bump skill-alerts

--- buildroot-external/package/skill-alerts/skill-alerts.mk
+++ buildroot-external/package/skill-alerts/skill-alerts.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-SKILL_ALERTS_VERSION = fa8b142fdd485a3c5cdb3757d10ca1b4ba019eb9
-SKILL_ALERTS_SITE = $(call github,NeonGeckoCom,skill-alerts,$(SKILL_ALERTS_VERSION))
+SKILL_ALERTS_VERSION = 83c9a717d6829492b9c95485de6ff0acc76ab962
+SKILL_ALERTS_SITE = $(call github,OpenVoiceOS,skill-alerts,$(SKILL_ALERTS_VERSION))
 SKILL_ALERTS_SETUP_TYPE = setuptools
 SKILL_ALERTS_LICENSE_FILES = LICENSE
 
