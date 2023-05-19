1a176180278b1187cfe56afe63227a8ee304bb03
j1nx <p.steenbergen@j1nx.nl>
2023-05-19 13:26:26 +0200

    Bump skill-ovos-personal

--- buildroot-external/package/skill-ovos-personal/skill-ovos-personal.mk
+++ buildroot-external/package/skill-ovos-personal/skill-ovos-personal.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-SKILL_OVOS_PERSONAL_VERSION = e4f04baee00b3b137ed317e1801e41f6d885cc91
+SKILL_OVOS_PERSONAL_VERSION = 7ca20a6e9319eb37f4caacdf2589fc1568b783d3
 SKILL_OVOS_PERSONAL_SITE = $(call github,OpenVoiceOS,ovos-skill-personal,$(SKILL_OVOS_PERSONAL_VERSION))
 SKILL_OVOS_PERSONAL_SETUP_TYPE = setuptools
 SKILL_OVOS_PERSONAL_LICENSE_FILES = LICENSE
+SKILL_OVOS_PERSONAL_ENV = MYCROFT_LOOSE_REQUIREMENTS=true
 
 $(eval $(python-package))
