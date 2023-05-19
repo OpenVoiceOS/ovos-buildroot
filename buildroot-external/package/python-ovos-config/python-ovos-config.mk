01c013b68e17311c38572b0ee6f0e8fac96ec767
j1nx <p.steenbergen@j1nx.nl>
2023-05-19 10:31:07 +0200

    Bump ovos-config

--- buildroot-external/package/python-ovos-config/python-ovos-config.mk
+++ buildroot-external/package/python-ovos-config/python-ovos-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_CONFIG_VERSION = 7df52cac6cbe369de43b06abb64e46cc83a29b7e
+PYTHON_OVOS_CONFIG_VERSION = 1b93f1a8806877777dc91f1598b01c14cb7d092c
 PYTHON_OVOS_CONFIG_SITE = $(call github,OpenVoiceOS,ovos-config,$(PYTHON_OVOS_CONFIG_VERSION))
 PYTHON_OVOS_CONFIG_SETUP_TYPE = setuptools
 PYTHON_OVOS_CONFIG_LICENSE_FILES = LICENSE
