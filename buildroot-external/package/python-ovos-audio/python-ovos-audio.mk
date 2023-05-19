f1ac07cc25f73d59160b7da3104bef7635c0c206
j1nx <p.steenbergen@j1nx.nl>
2023-05-19 10:20:38 +0200

    Bump ovos-audio

--- buildroot-external/package/python-ovos-audio/python-ovos-audio.mk
+++ buildroot-external/package/python-ovos-audio/python-ovos-audio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OVOS_AUDIO_VERSION = 3805078c09e46e8827e4eab93e1c943e5599a3e4
+PYTHON_OVOS_AUDIO_VERSION = 3903c8cbb0072fc03a260020b5ef4e6e6b823377
 PYTHON_OVOS_AUDIO_SITE = $(call github,OpenVoiceOS,ovos-audio,$(PYTHON_OVOS_AUDIO_VERSION))
 PYTHON_OVOS_AUDIO_SETUP_TYPE = setuptools
 PYTHON_OVOS_AUDIO_LICENSE_FILES = LICENSE
