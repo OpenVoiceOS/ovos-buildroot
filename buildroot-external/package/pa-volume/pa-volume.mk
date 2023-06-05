bc0638ff6f26f919e3f14b5430cab37ad11b456d
Val Orekhov <vorekhov@withum.com>
2023-06-05 13:47:13 -0400

    buildroot-external/package/pa-config: fix build
    https://github.com/buildroot/buildroot/blob/35dea785ef6a4c3f3faa91b20da60aef15d1bcdd/Makefile#L386
    unsets PKG_CONFIG_PATH which is needed by `pkg-config` tool used in this package.

--- buildroot-external/package/pa-volume/pa-volume.mk
+++ buildroot-external/package/pa-volume/pa-volume.mk
@@ -8,7 +8,7 @@ PA_VOLUME_VERSION = cfbff58992f6ca091c4c5784a6eb25bc422d7ae8
 PA_VOLUME_SITE = $(call github,rhaas80,pa_volume,$(PA_VOLUME_VERSION))
 
 define PA_VOLUME_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 define PA_VOLUME_INSTALL_TARGET_CMDS
