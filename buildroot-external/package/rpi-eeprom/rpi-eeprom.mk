edc70b2f4816a231df08ecc3c7f596b7595863cb
Bohdan Buinich <bohdan.buinich@gmail.com>
2024-01-07 18:44:52 +0200

    [RPI] rpi-eeprom: Future support for RPI5

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -3,12 +3,20 @@
 # rpi-eeprom
 #
 #############################################################
-RPI_EEPROM_VERSION = fa281d3be17d1edd2cc10b1d9479cc6b19b96ea8
+RPI_EEPROM_VERSION = 9e0bffb2916d6f31ae454a365bb1b563ee14bf97
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
 RPI_EEPROM_INSTALL_IMAGES = YES
-RPI_EEPROM_FIRMWARE_PATH = firmware/stable/pieeprom-2023-05-11.bin
+
+# Conditional firmware path based on kernel defconfig
+ifeq ($(BR2_LINUX_KERNEL_DEFCONFIG), "bcm2711") # Raspberry Pi 3/4
+    RPI_EEPROM_FIRMWARE_PATH = firmware-2711/stable/pieeprom-2023-05-11.bin
+else ifeq ($(BR2_LINUX_KERNEL_DEFCONFIG), "bcm2712") # Raspberry Pi 5
+    RPI_EEPROM_FIRMWARE_PATH = firmware-2712/stable/pieeprom-2024-01-05.bin
+else
+    $(error Unsupported Raspberry Pi model for RPI_EEPROM_FIRMWARE_PATH)
+endif
 
 define RPI_EEPROM_BUILD_CMDS
 	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --out $(@D)/default.conf
