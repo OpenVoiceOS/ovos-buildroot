0f7f40a50e777cdba3f00be71747f0d3d4de2984
Bohdan Buinich <bohdan.buinich@gmail.com>
2024-01-09 19:17:05 +0200

    It looks better

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -3,19 +3,16 @@
 # rpi-eeprom
 #
 #############################################################
-RPI_EEPROM_VERSION = 9e0bffb2916d6f31ae454a365bb1b563ee14bf97
+RPI_EEPROM_VERSION = 8855da988935f0c09ed4cc772103d4c5408a1afb
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
 RPI_EEPROM_INSTALL_IMAGES = YES
 
-# Conditional firmware path based on kernel defconfig
-ifeq ($(BR2_LINUX_KERNEL_DEFCONFIG), "bcm2711") # Raspberry Pi 3/4
-    RPI_EEPROM_FIRMWARE_PATH = firmware-2711/stable/pieeprom-2023-05-11.bin
-else ifeq ($(BR2_LINUX_KERNEL_DEFCONFIG), "bcm2712") # Raspberry Pi 5
-    RPI_EEPROM_FIRMWARE_PATH = firmware-2712/stable/pieeprom-2024-01-05.bin
-else
-    $(error Unsupported Raspberry Pi model for RPI_EEPROM_FIRMWARE_PATH)
+ifeq ($(BR2_PACKAGE_RPI_EEPROM_RPI4),y)
+	RPI_EEPROM_FIRMWARE_PATH = firmware-2711/stable/pieeprom-2023-05-11.bin
+else ifeq ($(BR2_PACKAGE_RPI_EEPROM_RPI5),y) # Raspberry Pi 5
+	RPI_EEPROM_FIRMWARE_PATH = firmware-2712/stable/pieeprom-2024-01-05.bin
 endif
 
 define RPI_EEPROM_BUILD_CMDS
