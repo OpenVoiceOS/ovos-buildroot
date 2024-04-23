35b7a5bf6302aea4f3b21385ae6ff7e77bdcb411
j1nx <p.steenbergen@j1nx.nl>
2024-04-23 12:32:37 +0000

    [RPI] Bump vocalfusion driver and XMOS 3510 INT SPI firmware

--- buildroot-external/package/vocalfusion/vocalfusion.mk
+++ buildroot-external/package/vocalfusion/vocalfusion.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VOCALFUSION_VERSION = b56f5c8751918a0a2505b9ae94631d6b72b8e7b0
+VOCALFUSION_VERSION = 5ca7f45cb5b679859fb251fdc870571a75dfb0a7
 VOCALFUSION_SITE = $(call github,OpenVoiceOS,VocalFusionDriver,$(VOCALFUSION_VERSION))
 VOCALFUSION_MODULE_SUBDIRS = driver
 
@@ -17,8 +17,8 @@ define VOCALFUSION_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/usr/sbin/xvf3510-flash
 
 	mkdir -p $(TARGET_DIR)/usr/lib/firmware/xvf3510
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/vocalfusion/app_xvf3510_int_spi_boot_v4_1_0.bin \
-                $(TARGET_DIR)/usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_1_0.bin
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_OPENVOICEOS_PATH)/package/vocalfusion/app_xvf3510_int_spi_boot_v4_2_0.bin \
+                $(TARGET_DIR)/usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_2_0.bin
 endef
 
 $(eval $(generic-package))
